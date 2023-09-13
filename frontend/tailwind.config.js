ffeab1b32c2166ebbabcac373ee87593f152a920
Lirae Que Data <liraedata59@gmail.com>
2023-09-13 22:22:48 +0800

    Add screens configuration in tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,7 +6,16 @@ export default {
   ],
   darkMode : "class",
   theme: {
-    extend: {},
+    extend: {
+      screens : {
+        "xs" : "360px",
+        "sm" : "640px",
+        "md" : "768px",
+        "lg" : "1024px",
+        "xl" : "1280px",
+        "2xl" : "1536px"
+      }
+    },
   },
   plugins: [],
 }
