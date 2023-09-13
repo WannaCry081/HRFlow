07bb2609b44ec62145a6ab537820b3bd158a2251
Lirae Que Data <liraedata59@gmail.com>
2023-09-13 22:32:24 +0800

    Add resolve configuration to vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve : {
+    alias : {
+      src : "/src",
+      components : "/src/components",
+      pages : "/src/pages",
+      services : "/src/services",
+      utils : "/src/utils",
+      constants : "/src/constants",
+      assets : "/src/assets"
+    }
+  }
 })
