916b8f9516be6189247ae46fc47450b0526e9e73
Lirae Que Data <liraedata59@gmail.com>
2023-09-13 23:17:43 +0800

    Add routes to App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
 const App = () => {
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" index element={null} />
+      <Route path="/*" element={null} />
+    </Routes>
   );
 };
 
