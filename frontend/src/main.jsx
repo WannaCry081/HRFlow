ee779be1e62af0f880b895e9ef332f7cb5be72b0
Lirae Que Data <liraedata59@gmail.com>
2023-09-13 23:17:23 +0800

    Add browser router to root

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import BrowserRouter from "react-router-dom";
 import App from "src/App";
 import "./styles/main.css";
-
 
 const rootElement = document.querySelector("#root");
 
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
