16e75137902252fc8ff9f478936138881f65fcc2
Lirae Que Data <liraedata59@gmail.com>
2023-09-14 00:02:35 +0800

    Add auth controller

---
+++ backend/HRIS/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRIS.Controllers
+{
+    [ApiController]
+    [Route("/api/auth")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost]
+        [Route("/register")]
+        public IActionResult Register()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("/login")]
+        public IActionResult Login()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost]
+        [Route("/forgot-password")]
+        public IActionResult ForgotPassword()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- backend/HRIS/Program.cs
+++ backend/HRIS/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
