5d08179e4e8b6c7ba5da59db48126dd6e030ea32
Gerald Carido <geraldcastcarido@gmail.com>
2023-09-17 01:19:08 +0800

    Added DataContext SQL ready

--- backend/HRIS/Program.cs
+++ backend/HRIS/Program.cs
@@ -1,5 +1,7 @@
+using HRIS.Context;
 using HRIS.Repositories.AuthRepository;
 using HRIS.Services.AuthService;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +12,12 @@ builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//DATACONTEXT SQL
+//builder.Services.AddDbContext<DataContext>(options =>
+//{
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+//});
 
 //Scoped Services and Repository
 builder.Services.AddScoped<IAuthService, AuthService>();
