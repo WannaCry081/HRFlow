3aa8392d223f637fb990c7181aa8ac413b5aaae7
Gerald Carido <geraldcastcarido@gmail.com>
2023-09-16 20:56:04 +0800

    Added constructor and Authservice interface inheritance

--- backend/HRIS/Services/AuthService/AuthService.cs
+++ backend/HRIS/Services/AuthService/AuthService.cs
@@ -1,6 +1,17 @@
-﻿namespace HRIS.Services.AuthService
+﻿using HRIS.Repositories.AuthRepository;
+using AutoMapper;
+
+namespace HRIS.Services.AuthService
 {
-    public class AuthService
+    public class AuthService : IAuthService
     {
+        private readonly IAuthRepository _repository;
+        private readonly IMapper _mapper;
+
+        public AuthService(IAuthRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
     }
 }
