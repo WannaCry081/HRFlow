6f38f9d9c18d79656d4cc6f1ead5a44d442fc992
Gerald Carido <geraldcastcarido@gmail.com>
2023-09-16 20:52:13 +0800

    Added AutoMapper and two way mapping

--- backend/HRIS/mappings/AuthMapper.cs
+++ backend/HRIS/mappings/AuthMapper.cs
@@ -1,6 +1,15 @@
-﻿namespace HRIS.Mappings
+﻿using AutoMapper;
+using HRIS.Dtos;
+using HRIS.Models.AuthModels;
+
+namespace HRIS.Mappings
 {
-    public class AuthMapper
+    public class AuthMapper : Profile
     {
+        public AuthMapper() 
+        {
+            //Two way mapping
+            CreateMap<ForgotPassword, ForgotPasswordDto>().ReverseMap();
+        }
     }
 }
