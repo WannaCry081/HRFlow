3d465c09f2f3ca0ce6fe5fbffae2789b1e101e95
Gerald Carido <geraldcastcarido@gmail.com>
2023-09-17 01:13:07 +0800

    Datacontext ready for tables

--- backend/HRIS/context/DataContext.cs
+++ backend/HRIS/context/DataContext.cs
@@ -1,6 +1,15 @@
-﻿namespace HRIS.Context
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HRIS.Context
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        //USER table
+        // public DbSet<User> Users { get; set; }
     }
 }
