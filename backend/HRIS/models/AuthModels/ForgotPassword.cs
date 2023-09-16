56abe8bf551792cd0ff738d976e27e36c05a3926
Gerald Carido <geraldcastcarido@gmail.com>
2023-09-17 01:16:31 +0800

    Created fields and annotations

--- backend/HRIS/models/AuthModels/ForgotPassword.cs
+++ backend/HRIS/models/AuthModels/ForgotPassword.cs
@@ -13,22 +13,12 @@ namespace HRIS.Models.AuthModels
         public string Email { get; set; } = string.Empty;
 
         //Name field annotations
-        [Required(ErrorMessage = "Name input is required.")]
-        [MinLength(1, ErrorMessage = "Name input is too short! Please provide at least 1 character.")]
-        [MaxLength(35, ErrorMessage = "Name input is too long! Please provide at most 35 characters.")]
+        [Required(ErrorMessage = "Name inputted is required.")]
+        [MinLength(1, ErrorMessage = "Name inputted is too short! Please provide at least 1 character.")]
+        [MaxLength(35, ErrorMessage = "Name inputted is too long! Please provide at most 35 characters.")]
         public string Name { get; set; } = string.Empty;
 
-        //Password field annotations
-        [Required(ErrorMessage = "Password is required.")]
-        [PasswordPropertyText]
-        [MinLength(8, ErrorMessage = "Password is too short! Please provide at least 8 characters")]
-        [MaxLength(16, ErrorMessage = "Password is too long! Please provide at most 16 characters.")]
-        public string Password { get; set; } = string.Empty;
-
-        //Confirm Passowrd field annotations
-        [Required(ErrorMessage = "Confirm Password is required.")]
-        [PasswordPropertyText]
-        [Compare("Password", ErrorMessage = "Password does not match with the password field.")]
-        public string ConfirmPassword { get; set; } = string.Empty;
+        //Use for Token Generation
+        public string Token { get; set; } = string.Empty;
     }
 }
