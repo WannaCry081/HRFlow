144d7f0ad6166eaa1baf675b269e10fa555a716d
shielamae02 <shiela.mlepon@gmail.com>
2023-09-20 23:09:16 +0800

    Minor fixes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" index element={<LandingPage />} />
-      <Route path="/*" element={<LandingPage />} />
+      <Route path="/home" element={<LandingPage />} />
     </Routes>
   );
 };
