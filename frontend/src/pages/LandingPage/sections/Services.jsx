6b0a12ca7ef1149bb3f4a4e0951c74476f9243e7
shielamae02 <shiela.mlepon@gmail.com>
2023-09-20 23:02:38 +0800

    Add services section

---
+++ frontend/src/pages/LandingPage/sections/Services.jsx
@@ -0,0 +1,62 @@
+import React, { forwardRef } from "react";
+import Bell from "../../../assets/svg/icons/Bell.svg";
+import Clock from "../../../assets/svg/icons/Clock.svg";
+import Coin from "../../../assets/svg/icons/Coin.svg";
+import Folder from "../../../assets/svg/icons/Folder.svg";
+import Megaphone from "../../../assets/svg/icons/Megaphone.svg";
+import Person from "../../../assets/svg/icons/Person.svg";
+import Thunder from "../../../assets/svg/icons/Thunder.svg";
+import FeatureCard from "../components/FeatureCard";
+
+const Services = forwardRef((props, ref) => {
+    return (
+        <section ref={ref} className="flex flex-col py-24 xl:py-22 px-4 justify-center">
+            <div className="w-full max-w-screen-xl mx-auto">
+                <div className="pt-2 pb-6">
+                    <img src={Thunder} className="h-10" />
+                    <h1 className="leading-tight text-jetblack font-lato font-bold text-3xl pt-2 pb-2">
+                        Convenient and optimized HR processes
+                    </h1>
+                    <p className="leading-tight text-gray-700 font-poppins text-xs">
+                        HR Flow offers variety of services for the ease of use of both HR professionals and employees
+                    </p>
+                </div>
+                <div className="grid gap-3 md:grid-cols-2 xl:grid-cols-3">
+                    <FeatureCard
+                        featureIcon={Folder}
+                        featureTitle="Employee Records Management"
+                        featureDescription="Maintain a comprehensive database of employee information, including personal details, contact information, and employment history."
+                    />
+                    <FeatureCard
+                        featureIcon={Person}
+                        featureTitle="Employee Self-service"
+                        featureDescription="Allow employees to update their personal information and submit requests and documents."
+                    />
+                    <FeatureCard
+                        featureIcon={Coin}
+                        featureTitle="Payroll, Benefits, and Compensation Management"
+                        featureDescription="Automate payroll calculations, tax deductions, and generate paychecks, manages employee benefits, defines and manages salary structures, bonuses, and incentives."
+                    />
+                    <FeatureCard
+                        featureIcon={Clock}
+                        featureTitle="Time and Attendance Tracking"
+                        featureDescription="Record and monitor employee attendance and work hours, including clock-in/clock-out features."
+                    />
+                    <FeatureCard
+                        featureIcon={Megaphone}
+                        featureTitle="Recruitment and Applicant Tracking"
+                        featureDescription="Accept applications and track candidates throughout the hiring process."
+                    />
+                    <FeatureCard
+                        featureIcon={Bell}
+                        featureTitle="Notifications and Alert"
+                        featureDescription="Send automated notifications for important announcements and upcoming events to employees."
+                    />
+                </div>
+            </div>
+            
+        </section>
+    );
+});
+
+export default Services;
