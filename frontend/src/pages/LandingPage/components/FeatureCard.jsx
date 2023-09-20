cea4a575964e3ff8bf11924256ca8e86855ff904
shielamae02 <shiela.mlepon@gmail.com>
2023-09-20 23:04:12 +0800

    Add feature card component

---
+++ frontend/src/pages/LandingPage/components/FeatureCard.jsx
@@ -0,0 +1,17 @@
+const FeatureCard = (props) => {
+    return (
+        <div>
+            <div className="border-[0.5px] border-gray-200 shadow-md rounded-3xl p-4 xl:p-6 h-72 bg-pastel-secondary flex flex-col items-start justify-center">
+                <img src={props.featureIcon} className="h-12" />
+                <h1 className="leading-tight text-jetblack font-lato font-bold text-2xl pt-6 pb-3 xl:text-xl">
+                    {props.featureTitle}
+                </h1>
+                <p className="leading-tight text-gray-700 font-poppins text-sm xl:text-xs">
+                    {props.featureDescription}
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default FeatureCard;
