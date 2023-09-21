4b25863304febbea439fd327cbc236d9e9d73a0b
Lirae Que Data <liraedata59@gmail.com>
2023-09-21 23:47:34 +0800

    Add new repository class methods

--- backend/HRIS/Repositories/AuthRepository/IAuthRepository.cs
+++ backend/HRIS/Repositories/AuthRepository/IAuthRepository.cs
@@ -1,10 +1,13 @@
-﻿using HRIS.Models.AuthModels;
+﻿using HRIS.Models;
+using HRIS.Dtos;
+using HRIS.Models.AuthModels;
 
 namespace HRIS.Repositories.AuthRepository
 {
     public interface IAuthRepository
     {
+        Task<bool> RegisterUser(User request);
+        Task<bool> LoginUser(User request);
         Task<string> SendEmail(ForgotPassword request);
-        
     }
 }
