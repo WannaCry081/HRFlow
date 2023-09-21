43a5eab89e0ccec435a4bfaf14c69db4db2f6c01
Lirae Que Data <liraedata59@gmail.com>
2023-09-21 23:13:47 +0800

    Add fields to RegisterUserDto.cs file

--- backend/HRIS/dtos/RegisterUserDto.cs
+++ backend/HRIS/dtos/RegisterUserDto.cs
@@ -1,6 +1,26 @@
-﻿namespace HRIS.Dtos
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRIS.Dtos
 {
     public class RegisterUserDto
     {
+        [Required(ErrorMessage = "First Name is required.")]
+        [StringLength(100, ErrorMessage = "First Name must be between {2} and {1} characters long.", MinimumLength = 2)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        [StringLength(100, ErrorMessage = "Last Name must be between {2} and {1} characters long.", MinimumLength = 2)]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email Address is required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        [StringLength(150, ErrorMessage = "Email Address must be between {2} and {1} characters long.", MinimumLength = 3)]
+        public string Email { get; set; } = string.Empty;
+
+        [PasswordPropertyText]
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(150, ErrorMessage = "Password must be between {2} and {1} characters long.", MinimumLength = 8)]
+        public string PasswordSalt { get; set; } = string.Empty;
     }
 }
