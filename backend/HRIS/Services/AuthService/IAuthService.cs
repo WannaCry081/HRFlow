d6c8495980c886d08f4a789a59fd739bfadcdb46
Lirae Que Data <liraedata59@gmail.com>
2023-09-21 23:27:07 +0800

    Add new auth service class method

--- backend/HRIS/Services/AuthService/IAuthService.cs
+++ backend/HRIS/Services/AuthService/IAuthService.cs
@@ -4,6 +4,8 @@ namespace HRIS.Services.AuthService
 {
     public interface IAuthService
     {
+        Task<string> RegisterUser(RegisterUserDto request);
+        Task<string> LoginUser(LoginUserDto request);
         Task<string> SendEmail(ForgotPasswordDto request);
     }
 }
