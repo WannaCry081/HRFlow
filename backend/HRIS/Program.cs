310242927ceba11646ac97af48d1a68c245e2215
Lirae Que Data <liraedata59@gmail.com>
2023-09-21 21:08:14 +0800

    Remove unnecessary comments in program file

--- backend/HRIS/Program.cs
+++ backend/HRIS/Program.cs
@@ -1,34 +1,24 @@
+using Microsoft.EntityFrameworkCore;
 using HRIS.Context;
 using HRIS.Repositories.AuthRepository;
 using HRIS.Services.AuthService;
-using Microsoft.EntityFrameworkCore;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//DATACONTEXT SQL
-//builder.Services.AddDbContext<DataContext>(options =>
-//{
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
-//});
-
-//Scoped Services and Repository
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 
-//Automapper dependency injection
 builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
