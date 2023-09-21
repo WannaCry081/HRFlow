1ceb9ed4f1da4fe0a9ac9feffbc0dd3dfcbe372b
Lirae Que Data <liraedata59@gmail.com>
2023-09-21 21:52:54 +0800

    Add fields to LoginUserDto.cs file

--- backend/HRIS/dtos/LoginUserDto.cs
+++ backend/HRIS/dtos/LoginUserDto.cs
@@ -1,6 +1,18 @@
-﻿namespace HRIS.Dtos
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRIS.Dtos
 {
     public class LoginUserDto
     {
+        [Required(ErrorMessage = "Email Address is required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        [StringLength(150, ErrorMessage = "Email Address must be between {2} and {1} characters long.", MinimumLength = 3)]
+        public string Email { get; set; } = string.Empty;
+
+        [PasswordPropertyText]
+        [Required(ErrorMessage = "Password is required.")]
+        [StringLength(150, ErrorMessage = "Password must be between {2} and {1} characters long.", MinimumLength = 8)]
+        public string Password { get; set; } = string.Empty;
     }
 }
