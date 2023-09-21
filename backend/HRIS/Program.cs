4321b5da8c3dcdf8855ab2b3a9f8a994e96d9ad8
Lirae Que Data <liraedata59@gmail.com>
2023-09-21 21:14:23 +0800

    Add configuration to sql server

--- backend/HRIS/Program.cs
+++ backend/HRIS/Program.cs
@@ -3,19 +3,23 @@ using HRIS.Context;
 using HRIS.Repositories.AuthRepository;
 using HRIS.Services.AuthService;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
+builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddTransient<DataContext>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
