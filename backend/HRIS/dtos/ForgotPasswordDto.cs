10f7ca3d894c87cd6aa3ce387fb6a33bf331dff4
Lirae Que Data <liraedata59@gmail.com>
2023-09-23 01:11:04 +0800

    Remove name property in ForgotPasswordDto

--- backend/HRIS/dtos/ForgotPasswordDto.cs
+++ backend/HRIS/dtos/ForgotPasswordDto.cs
@@ -8,9 +8,5 @@ namespace HRIS.Dtos
         [Required(ErrorMessage = "Email Address is required.")]
         [StringLength(150, ErrorMessage = "Email Address must be between {2} and {1} characters long.", MinimumLength = 3)]
         public string Email { get; set; } = string.Empty;
-
-        [Required(ErrorMessage = "Name input is required.")]
-        [StringLength(100, ErrorMessage = "First Name must be between {2} and {1} characters long.", MinimumLength = 2)]
-        public string Name { get; set; } = string.Empty;
     }
 }
