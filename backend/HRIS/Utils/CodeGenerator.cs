f4c90bda9556c232635066267e370d67e8f70039
Lirae Que Data <liraedata59@gmail.com>
2023-09-22 19:21:12 +0800

    Add new digit code generator

---
+++ backend/HRIS/Utils/CodeGenerator.cs
@@ -0,0 +1,23 @@
+﻿namespace HRIS.Utils
+{
+    public class CodeGenerator
+    {
+        private readonly Random _random;
+
+        private string _chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private string _digits = "0123456789";
+
+        public CodeGenerator()
+        {
+            _random = new Random();
+        }
+
+        public string Digit(int length)
+        {
+            string digits = new string(Enumerable.Repeat(_digits, length)
+              .Select(s => s[_random.Next(s.Length)]).ToArray());
+
+            return digits;
+        }
+    }
+}
