a1930312df7bf4498fd341297459475eeb0706da
Lirae Que Data <liraedata59@gmail.com>
2023-09-23 01:05:07 +0800

    Remove ForgotPasswordDto to User mapping

--- backend/HRIS/Mappers/AuthMapper.cs
+++ backend/HRIS/Mappers/AuthMapper.cs
@@ -1,18 +1,15 @@
 ﻿using AutoMapper;
 using HRIS.Dtos;
 using HRIS.Models;
-using HRIS.Models.AuthModels;
 
 namespace HRIS.Mappers
 {
     public class AuthMapper : Profile
     {
-        public AuthMapper() 
+        public AuthMapper()
         {
             CreateMap<User, RegisterUserDto>();
             CreateMap<User, LoginUserDto>();
-            CreateMap<ForgotPassword, ForgotPasswordDto>()
-                .ReverseMap();
         }
     }
 }
