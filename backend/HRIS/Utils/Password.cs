9b60f60c3a49c92ff65a3590effbb64bac54d30e
Lirae Que Data <liraedata59@gmail.com>
2023-09-22 22:54:28 +0800

    Add new password encrypt class method

---
+++ backend/HRIS/Utils/Password.cs
@@ -0,0 +1,13 @@
+﻿namespace HRIS.Utils
+{
+    public class Password
+    {
+        public static void Encrypt(string password, out string passwordHash, out string passwordSalt)
+        {
+            passwordSalt = BCrypt.Net.BCrypt.GenerateSalt();
+            passwordHash = BCrypt.Net.BCrypt.HashPassword(password, passwordSalt);
+        }
+
+        
+    }
+}
