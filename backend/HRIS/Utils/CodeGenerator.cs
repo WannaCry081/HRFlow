e1f2d376b8a4fb472bd6f4ad6ca4271be7f33351
Lirae Que Data <liraedata59@gmail.com>
2023-09-22 22:58:43 +0800

    Update codegenerator class methods to static

--- backend/HRIS/Utils/CodeGenerator.cs
+++ backend/HRIS/Utils/CodeGenerator.cs
@@ -2,29 +2,22 @@
 {
     public class CodeGenerator
     {
-        private readonly Random _random;
+        private static string _chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private static string _digits = "0123456789";
 
-        private string _chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        private string _digits = "0123456789";
-
-        public CodeGenerator()
+        public static string Digit(int length)
         {
-            _random = new Random();
-        }
-
-        public string Digit(int length)
-        {
-            string digits = new string(Enumerable.Repeat(_digits, length)
-              .Select(s => s[_random.Next(s.Length)]).ToArray());
-
+            Random random = new();
+            string digits = new(Enumerable.Repeat(_digits, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
             return digits;
         }
 
-        public string AlphaNumeric(int length)
+        public static string AlphaNumeric(int length)
         {
-            string alphaNumeric = new string(Enumerable.Repeat(_chars + _digits, length)
-              .Select(s => s[_random.Next(s.Length)]).ToArray());
-
+            Random random = new();
+            string alphaNumeric = new(Enumerable.Repeat(_chars + _digits, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
             return alphaNumeric;
         }
     }
