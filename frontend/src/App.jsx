bc55c0335f178f42c8fe4be05fcbe6a86e0d9121
Lirae Que Data <liraedata59@gmail.com>
2023-09-24 22:59:53 +0800

    Add auth routes to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import LandingPage from "./pages/LandingPage/LandingPage";
+import LandingPage from "./pages/LandingPage";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" index element={<LandingPage />} />
       <Route path="/home" element={<LandingPage />} />
+      <Route path="/auth/register" element={<RegisterPage /> } />
+      <Route path="/auth/login" element={<LoginPage /> } />
     </Routes>
   );
 };
