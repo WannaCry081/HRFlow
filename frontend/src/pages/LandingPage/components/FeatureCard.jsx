db9235f6fb8e9e6f492266c152cf63714aec7860
shielamae02 <shiela.mlepon@gmail.com>
2023-09-25 04:03:36 +0800

    Minor changes to feature card

--- frontend/src/pages/LandingPage/components/FeatureCard.jsx
+++ frontend/src/pages/LandingPage/components/FeatureCard.jsx
@@ -1,6 +1,6 @@
 const FeatureCard = (props) => {
     return (
-        <div className="border-[0.5px] border-gray-200 shadow-md rounded-3xl px-4 xl:p-6 transition h-[22rem] md:h-[26rem] xl:h-96 lg:h-80 flex flex-col items-start justify-center text-center bg-secondary-pastel">
+        <div className="border-[0.5px] border-gray-200 shadow-lg rounded-3xl px-4 xl:p-6 transition h-[22rem] md:h-[26rem] xl:h-96 lg:h-80 flex flex-col items-start justify-center text-center bg-secondary-pastel">
             <div className="w-full h-1/5 flex items-center justify-center"> 
                 <img src={props.featureIcon} className="h-16 transition lg:h-18"/>
             </div>
@@ -10,7 +10,7 @@ const FeatureCard = (props) => {
                 </h1>
             </div>
             <div className="w-full h-1/3"> 
-                <p className="transition leading-tight text-gray-700 font-poppins text-xl md:text-[1.2rem] xl:text-xl xl:leading-tight xl:pt-4 text-">
+                <p className="transition leading-tight text-gray-700 font-poppins text-xl md:text-[1.2rem] xl:text-[1.18rem] xl:leading-tight xl:pt-4 2xl:pt-6 2xl:text-lg 2xl:leading-tight">
                     {props.featureDescription}
                 </p>
             </div>
