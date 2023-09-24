d68546a9526399c5442289654a275accac3933f9
shielamae02 <shiela.mlepon@gmail.com>
2023-09-25 03:57:42 +0800

    Modify services section content

--- frontend/src/pages/LandingPage/sections/Services.jsx
+++ frontend/src/pages/LandingPage/sections/Services.jsx
@@ -5,14 +5,14 @@ import Coin from "assets/svg/icons/Coin.svg";
 import Folder from "assets/svg/icons/Folder.svg";
 import Megaphone from "assets/svg/icons/Megaphone.svg";
 import Person from "assets/svg/icons/Person.svg";
-import Thunder from "assets/svg/icons/Thunder.svg";
+import Diamond from "assets/svg/icons/Diamond.svg";
 import FeatureCard from "../components/FeatureCard";
 import PurpleGradient from "assets/svg/Services_purple.svg";
 import PinkGradient from "assets/svg/Services_pink.svg";
 
 const Services = forwardRef((props, ref) => {
     return (
-        <section ref={ref} className="relative flex flex-col py-24 xl:py-22 px-4 justify-center">
+        <section ref={ref} className="relative flex flex-col px-4 justify-center scroll-m-32">
             <img
                 src={PurpleGradient}
                 className="absolute -z-10 top-24 left-16 md:-right-10 md:top-32 xl:left-60 xl:top-28"
@@ -21,21 +21,21 @@ const Services = forwardRef((props, ref) => {
                 src={PinkGradient}
                 className="absolute -z-20 top-10 -left-28 md:top-0 md:-left-32 xl:-left-10 xl:-top-6"
             />
-            <div className="w-full max-w-screen-xl mx-auto">
+            <div className="w-full max-w-screen-2xl mx-auto">
                 <div className="pt-2 pb-8">
-                    <img src={Thunder} className="h-12" />
-                    <h1 className="leading-tight text-jetblack font-lato font-extrabold  text-3xl md:text-4xl pt-2 pb-2">
-                        Convenient and optimized HR Processes
+                    <img src={Diamond} className="h-20"/>
+                    <h1 className="transition leading-none text-jetblack font-lato font-black text-5xl md:text-5xl py-6 pb-2 pr-10">
+                        Seamless HR Solutions
                     </h1>
-                    <p className="leading-tight text-gray-700 font-poppins text-xs">
-                        HR Flow offers variety of services for the ease of use of both HR professionals and employees
+                    <p className="leading-tight text-gray-700 font-poppins text-xl md:text-[1.2rem] py-2 2xl:text-2xl transition">
+                        HR Flow offers variety of services for the ease of use of both HR professionals and employees.
                     </p>
                 </div>
-                <div className="grid gap-3 md:grid-cols-2 xl:grid-cols-3">
+                <div className="grid gap-4 md:grid-cols-2 xl:grid-cols-3">
                     <FeatureCard
                         featureIcon={Folder}
                         featureTitle="Employee Records Management"
-                        featureDescription="Maintain a comprehensive database of employee information, including personal details, contact information, and employment history."
+                        featureDescription="Maintain a comprehensive database of employee information, including personal details and  contact information."
                     />
                     <FeatureCard
                         featureIcon={Person}
@@ -45,7 +45,7 @@ const Services = forwardRef((props, ref) => {
                     <FeatureCard
                         featureIcon={Coin}
                         featureTitle="Payroll, Benefits, and Compensation Management"
-                        featureDescription="Automate payroll calculations, tax deductions, and generate paychecks, manages employee benefits, defines and manages salary structures, bonuses, and incentives."
+                        featureDescription="Automate payroll calculations, tax deductions, and generate paychecks, manages employee benefits and salary structures."
                     />
                     <FeatureCard
                         featureIcon={Clock}
@@ -59,7 +59,7 @@ const Services = forwardRef((props, ref) => {
                     />
                     <FeatureCard
                         featureIcon={Bell}
-                        featureTitle="Notifications and Alert"
+                        featureTitle="Notifications and Alerts"
                         featureDescription="Send automated notifications for important announcements and upcoming events to employees."
                     />
                 </div>
