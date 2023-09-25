a586df123737e8d03850676e45655ea678e97b40
shielamae02 <shiela.mlepon@gmail.com>
2023-09-25 13:19:05 +0800

    Modify spacing in feature card

--- frontend/src/pages/LandingPage/components/FeatureCard.jsx
+++ frontend/src/pages/LandingPage/components/FeatureCard.jsx
@@ -4,13 +4,13 @@ const FeatureCard = (props) => {
             <div className="w-full h-1/5 flex items-center justify-center"> 
                 <img src={props.featureIcon} className="h-16 transition lg:h-18"/>
             </div>
-            <div className="w-full h-1/12 md:h-1/4 flex justify-center items-end"> 
-                <h1 className="transition text-jetblack font-lato font-extrabold text-[1.7rem] leading-tight md:text-2xl md:leading-none  xl:leading-none pt-2 pb-6 md:pb-4 xl:pb-2 2xl:pb-1 2xl:text-[1.7rem]">
+            <div className="w-full h-1/12 md:h-1/4 flex justify-center items-center"> 
+                <h1 className="transition text-jetblack font-lato font-extrabold text-[1.7rem] leading-tight md:text-2xl md:leading-none  xl:leading-none pt-2 pb-6 md:pb-4 xl:pb-2 2xl:px-3 2xl:text-2xl">
                     {props.featureTitle}
                 </h1>
             </div>
             <div className="w-full h-1/3"> 
-                <p className="transition leading-tight text-gray-700 font-poppins text-xl md:text-[1.2rem] xl:text-[1.18rem] xl:leading-tight xl:pt-4 2xl:pt-6 2xl:text-lg 2xl:leading-tight">
+                <p className="transition leading-tight text-gray-500 font-poppins text-xl md:text-[1.2rem] xl:text-[1.18rem] xl:leading-tight xl:pt-4 2xl:pt-4 2xl:text-base 2xl:leading-tight ">
                     {props.featureDescription}
                 </p>
             </div>
