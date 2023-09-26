3c77519afa9a7e1957c56d76760b58f7ddf40654
Lirae Que Data <liraedata59@gmail.com>
2023-09-26 12:31:06 +0800

    Add new dashboard page

---
+++ frontend/src/pages/DashboardPage/DashboardPage.jsx
@@ -0,0 +1,9 @@
+const DashboardPage = () => {
+    return (
+        <section className="h-screen w-screen flex items-center justify-center">
+            <h1>Dashboard</h1>
+        </section>
+    );
+};
+
+export default DashboardPage;
