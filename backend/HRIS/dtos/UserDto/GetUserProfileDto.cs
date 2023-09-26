65b9671a9dc9fb0d9935bffa7c50bb3b5e37cf5f
Lirae Que Data <liraedata59@gmail.com>
2023-09-27 00:42:47 +0800

    Add new GetUserProfileDto class

---
+++ backend/HRIS/dtos/UserDto/GetUserProfileDto.cs
@@ -0,0 +1,16 @@
+﻿namespace HRIS.Dtos.UserDto
+{
+    public class GetUserProfileDto
+    {
+        public string? FirstName { get; set; }
+        public string? MiddleName { get; set; }
+        public string? LastName { get; set; }
+        public string? Suffix { get; set; }
+        public int? Age { get; set; }
+        public string? MobileNumber { get; set; }
+        public string? LandlineNumber { get; set; } 
+        public string? PersonalEmail { get; set; } 
+        public string? CompanyEmail { get; set; }
+        public string? GroupCode { get; set; }
+    }
+}
