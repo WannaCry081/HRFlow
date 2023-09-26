62c5c4480e99cd837bdb994556d94e23bab2dba5
Lirae Que Data <liraedata59@gmail.com>
2023-09-27 00:51:56 +0800

    Add new GetUserById method implementation

--- backend/HRIS/Repositories/UserRepository/IUserRepository.cs
+++ backend/HRIS/Repositories/UserRepository/IUserRepository.cs
@@ -1,6 +1,9 @@
-﻿namespace HRIS.Repositories.UserRepository
+﻿using HRIS.Models;
+
+namespace HRIS.Repositories.UserRepository
 {
     public interface IUserRepository
     {
+        Task<User?> GetUserById(Guid id);
     }
 }
