05a4813bfba00b95e85a0e97902b418e59c40b3f
Lirae Que Data <liraedata59@gmail.com>
2023-09-26 21:09:24 +0800

    Add new user controller

---
+++ backend/HRIS/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace HRIS.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("/api/user")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+    }
+}
