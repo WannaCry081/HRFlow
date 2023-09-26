f63142b6ff7af1a52ebb8f0435ac5d251a67b30b
Lirae Que Data <liraedata59@gmail.com>
2023-09-27 01:01:07 +0800

    Add new GetUserProfile method

--- backend/HRIS/Controllers/UserController.cs
+++ backend/HRIS/Controllers/UserController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HRIS.Exceptions;
+using HRIS.Services.UserService;
+using HRIS.Utils;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HRIS.Controllers
 {
@@ -9,10 +12,36 @@ namespace HRIS.Controllers
     public class UserController : ControllerBase
     {
         private readonly ILogger<UserController> _logger;
-        
-        public UserController(ILogger<UserController> logger)
+        private readonly IUserService _userService;
+
+        public UserController(ILogger<UserController> logger, IUserService userService)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            _userService = userService ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpGet]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetUserProfile()
+        {
+            try
+            {
+                var userId = UserClaim.GetCurrentUser(HttpContext) ??
+                    throw new Exception();
+
+                var response = await _userService.GetUserProfile(userId);
+                return Ok(response);
+            }
+            catch (UserNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to get user information.", ex);
+                return NotFound("An error occurred while finding user.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical("", ex);
+                return Problem("");
+            }
         }
     }
 }
