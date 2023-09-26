353cc544ecd5b239ee3f007fb479ddbdce4d7cd1
Lirae Que Data <liraedata59@gmail.com>
2023-09-26 12:33:56 +0800

    Add api calls to login page

--- frontend/src/pages/LoginPage/LoginPage.jsx
+++ frontend/src/pages/LoginPage/LoginPage.jsx
@@ -1,22 +1,39 @@
 import { FcGoogle } from "react-icons/fc";
 import { useFormik } from "formik";
-import { Link } from "react-router-dom";
+import { Link, useNavigate} from "react-router-dom";
 import * as Yup from "yup";
+import { LoginUser } from "services/authService.js";
 import HRFlowLogo_Dark from "assets/svg/HRFlowLogo_Dark.svg";
 
 const LoginPage = () => {
+    const navigate = useNavigate();
     const formik = useFormik({
         initialValues : {
             email : "",
             password : ""
         },
         onSubmit : (values) => {
-            alert(values.email);
+            const Login = async () => {
+                try {
+                    const response = await LoginUser(values);
+                    sessionStorage.setItem("token", response);
+                    navigate("/dashboard", {replace : true});
+                } catch (error) {
+                    console.log(error.response);
+                }
+            };
+            Login();
         },
         validationSchema : Yup.object({
-            
+            email : Yup.string().required("Email Address is required.")
+                    .email("Invalid Email Address")
+                    .min(4, "Email Address must be at least 4 characters.")
+                    .max(150, "Email Address can be at most 150 characters."),
+            password : Yup.string().required("Password is required.")
+                    .min(8, "Password must be at least 8 characters.")
+                    .max(150, "Password can be at most 150 characters."),
         })
-    });
+    }); 
 
     return (
         <section className="h-screen w-screen">
@@ -24,11 +41,13 @@ const LoginPage = () => {
                 <div className="relative h-screen w-1/2">
                     <div className="mx-28 flex flex-col h-full">
                         <header className="py-8">
-                            <img
-                                src={HRFlowLogo_Dark}
-                                alt="HR Flow Logo"
-                                className="h-14"
-                            />
+                            <Link to="/">
+                                <img
+                                    src={HRFlowLogo_Dark}
+                                    alt="HR Flow Logo"
+                                    className="h-14"
+                                />
+                            </Link>
                         </header>
                         <section className="mx-32 flex-grow">
                             <div className="flex flex-col h-full justify-center">
@@ -48,6 +67,9 @@ const LoginPage = () => {
                                                 onBlur={formik.handleBlur}
                                                 className="border border-gray-200 bg-gray-100 text-lg w-full p-3 rounded-md focus:border focus:border-gray-400 focus:outline-none"
                                                 />
+                                        <div className="text-start text-red-500">
+                                            {formik.errors.email && formik.touched.email && formik.errors.email}
+                                        </div>
                                     </div>
 
                                     <div>
@@ -60,14 +82,21 @@ const LoginPage = () => {
                                                 onBlur={formik.handleBlur}
                                                 className="border border-gray-200 bg-gray-100 w-full p-3 text-lg rounded-md focus:border focus:border-gray-400 focus:outline-none"
                                                 />
-                                        <p className="mt-2 text-end text-lg text-gray-600 font-poppins">Forgot Password?</p>
+                                        <div className="text-start text-red-500">
+                                            {formik.errors.password && formik.touched.password && formik.errors.password}
+                                        </div>
+                                        
+                                        <p className="mt-2 text-end text-md text-gray-600 font-poppins">
+                                            <Link to="/auth/forgot-password">Forgot Password?</Link>
+                                        </p>
+
                                     </div>
                                 
                                     <div className=" flex flex-col gap-4">
-                                        <button className="bg-primary-light p-4 rounded-lg text-xl font-semibold text-white hover:bg-primary-dark shadow-md">Submit</button>
+                                        <button type="submit" className="bg-primary-light p-4 rounded-lg text-xl font-semibold text-white hover:bg-primary-dark shadow-md">Log In</button>
                                         <button className="bg-primary-pastel p-4 rounded-lg text-xl font-semibold text-primary-light flex items-center justify-center shadow-md">
-                                            <FcGoogle size={28} />
-                                            Sign in with Google
+                                            <FcGoogle size={28} className="mr-2" />
+                                            Continue with Google
                                         </button>
                                     </div>
 
