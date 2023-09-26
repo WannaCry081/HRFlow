21ac7d9606aed1cef777cd449d1a2d90072174ea
Lirae Que Data <liraedata59@gmail.com>
2023-09-27 00:43:46 +0800

    Add new UserMapper for user mapping

---
+++ backend/HRIS/Mappers/UserMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HRIS.Models;
+using HRIS.Dtos.UserDto;
+
+namespace HRIS.Mappers
+{
+    public class UserMapper : Profile
+    {
+        public UserMapper()
+        {
+            CreateMap<User, GetUserProfileDto>();
+        }
+    }
+}
