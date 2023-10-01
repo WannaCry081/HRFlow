b867369d6bc7eab0337f54a175d42784f32ce8b1
Lirae Que Data <liraedata59@gmail.com>
2023-10-01 20:20:03 +0800

    Update UpdateUserProfileDto fields

--- backend/HRIS/dtos/UserDto/UpdateUserProfileDto.cs
+++ backend/HRIS/dtos/UserDto/UpdateUserProfileDto.cs
@@ -4,19 +4,6 @@ namespace HRIS.Dtos.UserDto
 {
     public class UpdateUserProfileDto
     {
-        [StringLength(100, ErrorMessage = "First Name must be {1} characters long.")]
-        public string? FirstName { get; set; }
-
-        [StringLength(100, ErrorMessage = "Middle Name must be {1} characters long.")]
-        public string? MiddleName { get; set; }
-
-        [StringLength(100, ErrorMessage = "Last Name must be {1} characters long.")]
-        public string? LastName { get; set; }
-
-        [StringLength(10, ErrorMessage = "Suffix must be {1} characters long.")] 
-        public string? Suffix { get; set; }
-        public int? Age { get; set; }
-
         [StringLength(15, ErrorMessage = "Mobile Number must be {1} characters long.")]
         public string? MobileNumber { get; set; }
 
@@ -26,9 +13,5 @@ namespace HRIS.Dtos.UserDto
         [EmailAddress(ErrorMessage = "Please enter a valid personal email address.")]
         [StringLength(150, ErrorMessage = "Personal Email Address must be {1} characters long.")]
         public string? PersonalEmail { get; set; }
-
-        [EmailAddress(ErrorMessage = "Please enter a valid company email address.")]
-        [StringLength(150, ErrorMessage = "Company Email Address must be {1} characters long.")]
-        public string? CompanyEmail { get; set; }
     }
 }
