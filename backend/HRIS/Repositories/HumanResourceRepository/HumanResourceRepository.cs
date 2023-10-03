bb5fbca233af459cd649600c8ee665f8a335af09
shielamae02 <shiela.mlepon@gmail.com>
2023-10-03 23:30:19 +0800

    Add human resource repository methods

--- backend/HRIS/Repositories/HumanResourceRepository/HumanResourceRepository.cs
+++ backend/HRIS/Repositories/HumanResourceRepository/HumanResourceRepository.cs
@@ -1,6 +1,25 @@
-﻿namespace HRIS.Repositories.HumanResourceRepository
+﻿using HRIS.Context;
+using HRIS.Models;
+
+namespace HRIS.Repositories.HumanResourceRepository
 {
     public class HumanResourceRepository : IHumanResourceRepository
     {
+        private readonly DataContext _context;
+        
+        public HumanResourceRepository(DataContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public Task<bool> CreateEmployeeRecord(User employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateEmployeeRecord(User employee, User newEmployeeDetails)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
