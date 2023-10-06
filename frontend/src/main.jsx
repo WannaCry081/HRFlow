93fd3a79eae23199b9fe803659300aa82a8f5f43
Lirae Que Data <liraedata59@gmail.com>
2023-10-06 13:24:19 +0800

    Update styles import in Main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "@/App";
-import "@/styles/main.css";
+import "@Styles/main.css";
 
 const rootElement = document.querySelector("#root");
 
