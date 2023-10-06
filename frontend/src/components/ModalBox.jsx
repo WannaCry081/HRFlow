95378eea780286bd3703160fadba44e54fbcc520
Lirae Que Data <liraedata59@gmail.com>
2023-10-06 19:48:27 +0800

    Add adjustable top margin

--- frontend/src/components/ModalBox.jsx
+++ frontend/src/components/ModalBox.jsx
@@ -6,7 +6,7 @@ export const ModalBox = (prop) => {
     return (
         <div className="fixed z-10 h-screen w-screen top-0 left-0 bg-[#20262e80] p-4"
             onClick={prop.onCancel}>
-            <section className="max-w-[36rem] rounded-2xl bg-white mx-auto my-32 sm:my-56 p-6 sm:p-10" onClick={handleSectionClick}>
+            <section className={`${prop.top ? prop.top : "mt-32"} ${prop.topBreakpoint ? prop.topBreakpoint : "sm:mt-52"} max-w-[36rem] rounded-2xl bg-white mx-auto p-6 sm:p-10`} onClick={handleSectionClick}>
                 {prop.children}
             </section>
         </div>
