cc17d0435a4668b6014712ad70614dbbfcd5b601
shielamae02 <shiela.mlepon@gmail.com>
2023-10-09 23:59:09 +0800

    Add get employee to hr repository interface

--- backend/HRIS/Repositories/HumanResourceRepository/IHumanResourceRepository.cs
+++ backend/HRIS/Repositories/HumanResourceRepository/IHumanResourceRepository.cs
@@ -4,6 +4,7 @@ namespace HRIS.Repositories.HumanResourceRepository
 {
     public interface IHumanResourceRepository
     {
+        Task<User?> GetEmployee(Guid userId);
         Task<bool> CreateEmployeeRecord(User employee);
         Task<bool> UpdateEmployeeRecord(User updateEmployee);
        // Task<bool> UpdateEmployeeStatus(User updateEmployee); 
