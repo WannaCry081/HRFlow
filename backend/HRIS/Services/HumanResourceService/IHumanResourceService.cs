d95bb48cc171ae77ff6b0ae87467178516e6245e
shielamae02 <shiela.mlepon@gmail.com>
2023-10-10 00:05:25 +0800

    Add create and update employee record to hr service interface

--- backend/HRIS/Services/HumanResourceService/IHumanResourceService.cs
+++ backend/HRIS/Services/HumanResourceService/IHumanResourceService.cs
@@ -1,6 +1,11 @@
-﻿namespace HRIS.Services.HumanResourceService
+﻿using HRIS.dtos.EmployeeDto;
+using HRIS.Models;
+
+namespace HRIS.Services.HumanResourceService
 {
     public interface IHumanResourceService
     {
+        Task<User> CreateEmployeeRecord(UpsertEmployeeDto request);
+        Task<GetEmployeeDto> UpdateEmployeeRecord(Guid employeeId, UpsertEmployeeDto request);
     }
 }
