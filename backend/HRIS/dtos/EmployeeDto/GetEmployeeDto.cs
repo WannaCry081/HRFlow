33a69e8f421b589df66ea95606eb58a96bafb3a1
shielamae02 <shiela.mlepon@gmail.com>
2023-10-09 23:58:17 +0800

    Add get employee dto

--- backend/HRIS/dtos/EmployeeDto/GetEmployeeDto.cs
+++ backend/HRIS/dtos/EmployeeDto/GetEmployeeDto.cs
@@ -3,25 +3,25 @@
     public class GetEmployeeDto
     {
 
-		public string FirstName { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
 
-		public string MiddleName { get; set; } = string.Empty;
+        public string MiddleName { get; set; } = string.Empty;
 
-		public string LastName { get; set; } = string.Empty;
-		
-		public string Suffix { get; set; } = string.Empty;
-		public int Age { get; set; } = 0;
+        public string LastName { get; set; } = string.Empty;
 
-		public string MobileNumber { get; set; } = string.Empty;
+        public string Suffix { get; set; } = string.Empty;
+        public int Age { get; set; } = 0;
 
-		public string LandlineNumber { get; set; } = string.Empty;
+        public string MobileNumber { get; set; } = string.Empty;
 
-		public string PersonalEmail { get; set; } = string.Empty;
-		public string CompanyEmail { get; set; } = string.Empty;
+        public string LandlineNumber { get; set; } = string.Empty;
 
-		public string Role { get; set; } = string.Empty;
-		public string Status { get; set; } = string.Empty;
-		public DateTime CreatedAt { get; set; } = DateTime.Now;
-		public DateTime UpdatedAt { get; set; } = DateTime.Now;
-	}
+        public string PersonalEmail { get; set; } = string.Empty;
+        public string CompanyEmail { get; set; } = string.Empty;
+
+        public string Role { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+    }
 }
