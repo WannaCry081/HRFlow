2e391cb2576f9b1fc4d8ea760235728f76b5bd9c
Lirae Que Data <liraedata59@gmail.com>
2023-10-09 15:19:44 +0800

    Add boilerplate code to home page

---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+const Home = () => {
+    useEffect(() => {
+        // alert("Hello World");
+    }, []);
+    return (
+        <>
+            <h1>Home</h1>
+        </>
+    );
+};
+
+export default Home;
