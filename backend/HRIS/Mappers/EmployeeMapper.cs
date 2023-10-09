b441518bac59906a8c51d470d71e380416f69aac
shielamae02 <shiela.mlepon@gmail.com>
2023-10-09 22:52:51 +0800

    Add employee mapper

---
+++ backend/HRIS/Mappers/EmployeeMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using HRIS.dtos.EmployeeDto;
+using HRIS.Models;
+
+namespace HRIS.Mappers
+{
+    public class EmployeeMapper : Profile
+    {
+        public EmployeeMapper()
+        {
+            CreateMap<User, UpsertEmployeeDto>()
+                .ReverseMap();
+        }
+    }
+}
