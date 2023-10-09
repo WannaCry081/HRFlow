4a02b95a2462746718507bd0b22a1d1db953d451
shielamae02 <shiela.mlepon@gmail.com>
2023-10-09 23:23:27 +0800

    Add get employee dto

---
+++ backend/HRIS/dtos/EmployeeDto/GetEmployeeDto.cs
@@ -0,0 +1,27 @@
+﻿namespace HRIS.dtos.EmployeeDto
+{
+    public class GetEmployeeDto
+    {
+
+		public string FirstName { get; set; } = string.Empty;
+
+		public string MiddleName { get; set; } = string.Empty;
+
+		public string LastName { get; set; } = string.Empty;
+		
+		public string Suffix { get; set; } = string.Empty;
+		public int Age { get; set; } = 0;
+
+		public string MobileNumber { get; set; } = string.Empty;
+
+		public string LandlineNumber { get; set; } = string.Empty;
+
+		public string PersonalEmail { get; set; } = string.Empty;
+		public string CompanyEmail { get; set; } = string.Empty;
+
+		public string Role { get; set; } = string.Empty;
+		public string Status { get; set; } = string.Empty;
+		public DateTime CreatedAt { get; set; } = DateTime.Now;
+		public DateTime UpdatedAt { get; set; } = DateTime.Now;
+	}
+}
