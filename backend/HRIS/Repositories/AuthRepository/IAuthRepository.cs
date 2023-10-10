c15d171d029296f7c37fc7cd69df0266b8555bf9
shielamae02 <shiela.mlepon@gmail.com>
2023-10-10 18:43:15 +0800

    Add get user by id in authrepository interface

--- backend/HRIS/Repositories/AuthRepository/IAuthRepository.cs
+++ backend/HRIS/Repositories/AuthRepository/IAuthRepository.cs
@@ -7,6 +7,7 @@ namespace HRIS.Repositories.AuthRepository
         Task<bool> AddUser(User request);
         Task<bool> IsEmailExists(string email);
         Task<User?> GetUserByEmail(string email);
+        Task<User?> GetUserById(Guid id); 
         Task<bool> UpdateUserCode(User user, string code);
         Task<bool> UpdateUserPassword(User request, string email);
     }
