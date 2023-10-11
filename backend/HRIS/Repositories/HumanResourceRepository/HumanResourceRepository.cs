33c2811a46059cbe966e94d0c49bca5ed8d16871
shielamae02 <shiela.mlepon@gmail.com>
2023-10-11 17:29:50 +0800

    Update create employee record method in hr repository

--- backend/HRIS/Repositories/HumanResourceRepository/HumanResourceRepository.cs
+++ backend/HRIS/Repositories/HumanResourceRepository/HumanResourceRepository.cs
@@ -1,5 +1,7 @@
 ﻿using HRIS.Context;
+using HRIS.Exceptions;
 using HRIS.Models;
+using HRIS.Utils;
 using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,17 +16,37 @@ namespace HRIS.Repositories.HumanResourceRepository
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
-        public async Task<bool> CreateEmployeeRecord(User employee)
+        public async Task<bool> CreateEmployeeRecord(Guid id, User request)
         {
-            _context.Add(employee);
-            var result = await _context.SaveChangesAsync();
-            return result > 0;
+           
+            var hr = await _context.Users.Where(
+                c => c.Id.Equals(id)).FirstOrDefaultAsync();
+
+            var employee = await _context.Users.Where(
+                c => c.CompanyEmail.Equals(request.CompanyEmail)).FirstOrDefaultAsync();
+
+            if (employee is not null)
+            {
+                throw new UserExistsException("Employee is already recorded to the database.");
+            }
+
+            if (hr is null)
+            {
+                throw new UserNotFoundException("User not found.");
+            }
+
+            request.CreatedBy = hr.FirstName + " " + hr.LastName;
+            request.GroupCode = hr.GroupCode;
+            request.TeamId = hr.TeamId;
+
+            _context.Users.Add(request);
+            return await _context.SaveChangesAsync() > 0;
         }
 
 
-        public async Task<bool> UpdateEmployeeRecord(User updateEmployee, JsonPatchDocument<User> request)
+        public async Task<bool> UpdateEmployeeRecord(User user, JsonPatchDocument<User> request)
         {
-            request.ApplyTo(updateEmployee);
+            request.ApplyTo(user);
             var result = await _context.SaveChangesAsync();
             return result > 0;
         }
