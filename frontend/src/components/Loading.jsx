c3c1c64ee5d55a7f6989e345120f4a8b416d839a
Lirae Que Data <liraedata59@gmail.com>
2023-10-11 12:26:34 +0800

    Add new circular progress bar component

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -12,3 +12,14 @@ export const ProgressBar = (prop) => {
     );
 };
 
+export const CircularProgressBar = (prop) => {
+    const background = prop.background || "primary-light";
+    const foreground = prop.foreground || "primary-pastel";
+  
+    return (
+        <>
+            <div className={`relative animate-spin w-6 h-6 rounded-full border-2 border-${foreground} border-t-${background}`}></div>
+            {prop.children}
+        </>
+    );
+  };
