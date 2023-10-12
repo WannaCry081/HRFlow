6455d40ea618f2fb3908f1ddb77f4fb2afd31dfc
Lirae Que Data <liraedata59@gmail.com>
2023-10-12 21:17:00 +0800

    Add new create and join team methods

--- backend/HRIS/Controllers/UserController.cs
+++ backend/HRIS/Controllers/UserController.cs
@@ -17,8 +17,10 @@ namespace HRIS.Controllers
 
         public UserController(ILogger<UserController> logger, IUserService userService)
         {
-            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+            _logger = logger ??
+                throw new ArgumentNullException(nameof(logger));
+            _userService = userService ??
+                throw new ArgumentNullException(nameof(userService));
         }
 
         [HttpGet]
@@ -28,7 +30,7 @@ namespace HRIS.Controllers
             try
             {
                 var userId = UserClaim.GetCurrentUser(HttpContext) ??
-                    throw new UserNotFoundException("Invalid user.");   
+                    throw new UserNotFoundException("Invalid user.");
 
                 var response = await _userService.GetUserProfile(userId);
                 return Ok(response);
@@ -66,6 +68,77 @@ namespace HRIS.Controllers
             {
                 _logger.LogCritical("An error occurred while attempting to update user data.", ex);
                 return Problem(ex.Message);
+            }
+        }
+
+        [HttpPost("team")]
+        [Produces("application/json")]
+        public async Task<IActionResult> CreateTeam([FromBody] CreateTeamDto request)
+        {
+            try
+            {
+                var userId = UserClaim.GetCurrentUser(HttpContext) ??
+                  throw new UserNotFoundException("Invalid user.");
+
+                var response = await _userService.CreateTeam(userId, request);
+                if (!response)
+                {
+                    throw new TeamExistsException("Team already exists");
+                }
+                return Ok("Successfully created a team.");
+            }
+            catch (UserNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to get user information.", ex);
+                return NotFound("An error occurred while finding user.");
+            }
+            catch (TeamExistsException ex)
+            {
+                _logger.LogError("An error occurred while attempting to create a team.", ex);
+                return BadRequest("An error occurred while trying to create a team.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical("An error occurred while attempting to generate team code. ", ex);
+                return Problem("An error occurred while processing team code request. Please try again later.");
+            }
+        }
+
+        [HttpPut("team")]
+        [Produces("application/json")]
+        public async Task<IActionResult> JoinTeam([FromBody] JoinTeamDto request)
+        {
+            try
+            {
+                var userId = UserClaim.GetCurrentUser(HttpContext) ??
+                  throw new UserNotFoundException("Invalid user.");
+
+                var response = await _userService.JoinTeam(userId, request);
+                if (!response)
+                {
+                    throw new TeamExistsException("User already has a team.");
+                }
+                return Ok("Successfully joined team.");
+            }
+            catch (UserNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to get user information.", ex);
+                return NotFound("An error occurred while finding user.");
+            }
+            catch (TeamExistsException ex)
+            {
+                _logger.LogError("An error occurred while attempting to join a team.", ex);
+                return BadRequest("An error occurred while trying to join a team.");
+            }
+            catch (TeamNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to find the team.", ex);
+                return NotFound("An error occurred while finding the team.");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical("An error occurred while attempting to join a team. ", ex);
+                return Problem("An error occurred while processing team entry request. Please try again later.");
             }
         }
     }
