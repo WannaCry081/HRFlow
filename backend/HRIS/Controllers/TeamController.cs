67f35803f58998f8519ad36ca92817215c2def5d
Lirae Que Data <liraedata59@gmail.com>
2023-10-14 23:05:11 +0800

    Add new team controller

---
+++ backend/HRIS/Controllers/TeamController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRIS.Controllers
+{
+    [Authorize(Roles = "Human Resource")]
+    [ApiController]
+    [Route("/api/team")]
+    public class TeamController : ControllerBase
+    {
+        public TeamController()
+        {
+
+        }
+    }
+}
