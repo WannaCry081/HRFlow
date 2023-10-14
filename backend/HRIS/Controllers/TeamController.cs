94980ec07d60a3c66990813fa3814f0bf0986832
Lirae Que Data <liraedata59@gmail.com>
2023-10-14 23:32:24 +0800

    Add new join team method to team controller

--- backend/HRIS/Controllers/TeamController.cs
+++ backend/HRIS/Controllers/TeamController.cs
@@ -55,5 +55,42 @@ namespace HRIS.Controllers
                 return Problem("Internal server error.");
             }
         }
+        [HttpPut]
+        [Produces("application/json")]
+        public async Task<IActionResult> JoinTeam([FromBody] JoinTeamDto request)
+        {
+            try
+            {
+                var userId = UserClaim.GetCurrentUser(HttpContext) ??
+                  throw new UserNotFoundException("Invalid user's credential. Please try again.");
+
+                var response = await _teamService.JoinTeam(userId, request);
+                if (!response)
+                {
+                    throw new TeamExistsException("User already has a team. Please try again.");
+                }
+                return Ok("Successfully joined a team.");
+            }
+            catch (UserNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to get user information.", ex);
+                return NotFound(ex.Message);
+            }
+            catch (TeamExistsException ex)
+            {
+                _logger.LogError("An error occurred while attempting to join a team.", ex);
+                return BadRequest(ex.Message);
+            }
+            catch (TeamNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to find the team.", ex);
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical("An error occurred while attempting to join a team. ", ex);
+                return Problem("Internal server error.");
+            }
+        }
     }
 }
