adeb8a676272d53a6319f2b497e9a52e5106a23b
Lirae Que Data <liraedata59@gmail.com>
2023-10-15 10:47:29 +0800

    Add new update user profile method to user service

--- backend/HRIS/Services/UserService/UserService.cs
+++ backend/HRIS/Services/UserService/UserService.cs
@@ -3,6 +3,7 @@ using HRIS.Dtos.UserDto;
 using HRIS.Exceptions;
 using HRIS.Models;
 using HRIS.Repositories.UserRepository;
+using HRIS.Utils;
 
 namespace HRIS.Services.UserService
 {
@@ -23,6 +24,30 @@ namespace HRIS.Services.UserService
                 throw new UserNotFoundException("Invalid email address. Please try again.");
 
             return _mapper.Map<GetUserProfileDto>(response);
+        }
+
+        public async Task<GetUserProfileDto> UpdateUserPassword(Guid userId, UpdateUserPasswordDto request)
+        {
+            var user = await _userRepository.GetUserById(userId) ??
+                throw new UnauthorizedAccessException("Invalid email address. Please try again.");
+
+            if (!Password.Verify(user.PasswordHash, request.OldPassword))
+            {
+                throw new UnauthorizedAccessException("Invalid user's credentials. Please try again.");
+            }
+
+            if (!request.NewPassword.Equals(request.NewPassword))
+            {
+                throw new BadHttpRequestException("Password does not match. Please try again.");
+            }
+
+            Password.Encrypt(request.NewPassword, out string passwordHash, out string passwordSalt);
+            var isUserUpdated = await _userRepository.UpdateUserPassword(user, passwordHash, passwordSalt);
+            if (!isUserUpdated)
+            {
+                throw new Exception("Failed to update user's credential to database.");
+            }
+            return _mapper.Map<GetUserProfileDto>(user);
         }
 
         public async Task<GetUserProfileDto> UpdateUserProfile(Guid userId, UpdateUserProfileDto request)
