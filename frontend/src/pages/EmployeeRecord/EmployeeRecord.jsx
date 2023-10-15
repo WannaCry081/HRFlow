baa4d9652e2af172fdb9ec3a584d00c297397cfc
shielamae02 <shiela.mlepon@gmail.com>
2023-10-15 09:04:26 +0800

    Fix employee records table

--- frontend/src/pages/EmployeeRecord/EmployeeRecord.jsx
+++ frontend/src/pages/EmployeeRecord/EmployeeRecord.jsx
@@ -1,9 +1,10 @@
 import { LuSearch } from "react-icons/lu";
-import { MdAddCircle } from "react-icons/md";
+import { IoMdAdd } from "react-icons/io";
 import { GetEmployeeApi } from "@Services/employeeService";
 import React, { useEffect, useState } from "react";
 import useToggle from "@Hooks/useToggle";
-import AddEmployeeForm from "./Forms/AddEmployee";
+import AddEmployeeForm from "./Views/AddEmployee";
+import ViewEmployee from "./Views/ViewEmployee";
 
 const EmployeeRecord = () => {
     const token = sessionStorage.getItem("token");
@@ -18,7 +19,7 @@ const EmployeeRecord = () => {
             setEmployees(response.data);
         }
         getEmployees();
-    }, [ submit ])
+    }, [submit])
 
     const filteredEmployees = employees.filter((employee) => {
         const firstName = employee.firstName || "";
@@ -29,7 +30,6 @@ const EmployeeRecord = () => {
             lastName.toLowerCase()
         );
     })
-
     const groupedEmployees = filteredEmployees.reduce((groups, employee) => {
         const firstLetter = employee.firstName[0].toUpperCase();
         if (!groups[firstLetter]) {
@@ -39,95 +39,95 @@ const EmployeeRecord = () => {
         return groups;
     }, {});
 
+    const sortedGroupLetters = Object.keys(groupedEmployees).sort();
 
     return (
-        <section className="bg-primary-base w-full h-full">
-            
-            {addEmployee && <AddEmployeeForm onSetSubmit={onSetSubmit} submit={submit} onSetAddEmployee={onSetAddEmployee}/>}
-
-            <div className="w-full flex h-full pt-28">
-                <div className="w-[65%] h-full bg-primary-palePastel rounded-tl-[2rem] rounded-tr-[2rem] pt-12 px-8 flex flex-col">
-                    <div className="flex gap-4 justify-between">
-                        <div className="relative items-center w-96">
-                            <span className="absolute pl-3 inset-y-0 flex items-center">
-                                <LuSearch size={24} className="stroke-gray-400" />
-                            </span>
-                            <input
-                                placeholder="Search employee"
-                                className="text-lg rounded-lg border-primary-pastel p-2 pl-12 bg-white shadow-sm w-full border-collapse focus:border focus:outline-primary-light"
-                            />
-                        </div>
-                        <div 
-                            onClick={onSetAddEmployee}
-                            className="bg-primary-light w-48 relative flex justify-center items-center rounded-lg font-semibold text-white font-lato text-lg">
-                            <MdAddCircle size={26} className="mr-2 fill-white" />
-                            Add Employee
-                        </div>
-
-                    </div>
-                    <div className="flex-grow max-h-[780px] bg-white shadow-lg rounded-tl-2xl rounded-tr-2xl px-4 pt-4 mt-8">
-                        <div className="relative overflow-y-hidden flex h-full">
-                            <div className=" overflow-y-auto w-full h-full">
-                                {
-                                    employees.length === 0
-                                        ?
-                                        <>
-                                            <div className="flex flex-col justify-center items-center h-full p-12">
-                                                <p className='font-medium text-center'>No employees found.</p>
-                                            </div>
-                                        </>
-
-                                        : <table className="w-full text-lg text-left text-gray-700 dark:text-gray-400 relative">
-                                            <thead className="uppercase dark:text-gray-500 self-center sticky top-0 w-full bg-white ">
-                                                <tr className="text-sm ">
-                                                    <th scope="col" className="text-primary-light px-6 py-3 w-2/5 hidden md:table-cell">
-                                                        Name
-                                                    </th>
-                                                    <th scope="col" className="px-6 py-3 text-primary-light hidden md:table-cell" style={{ width: '25%' }}>
-                                                        Company Email
-                                                    </th>
-                                                </tr>
-                                            </thead>
-                                            <tbody className="text-gray-500 h-full">
-                                                {Object.keys(groupedEmployees).map((groupLetter) => (
-                                                    <React.Fragment key={groupLetter}>
-                                                        <tr className="bg-primary-palePastel border-none">
-                                                            <td colSpan="3" className="px-6 py-1 my-8 text-sm font-semibold">
-                                                                {groupLetter}
-                                                            </td>
-                                                        </tr>
-                                                        {groupedEmployees[groupLetter].map((employee) => (
-                                                            <tr
-                                                                key={employees.id}
-                                                                className={`hover:bg-paleBlue cursor-pointer h-full`}
-                                                            >
-                                                                <td className="px-6 py-2 h-full font-poppins text-gray-600 flex items-center text-ellipsis">
-                                                                    <div className="flex items-center justify-center font-medium h-14 w-14 bg-secondary-pastel text-brown mr-8 2xl:mr-6 rounded-xl shadow-md">
-                                                                        <div className="h-full w-full flex items-center justify-center text-secondary-light font-poppins">
-                                                                            {employee.firstName[0]}{employee.lastName[0]}
-                                                                        </div>
-                                                                    </div>
-                                                                    {`${employee.firstName} ${employee.lastName}`}
-                                                                </td>
-                                                                <td className="hidden md:table-cell px-6 py-2 font-poppins text-gray-600" style={{ width: '25%' }}>
-                                                                    {employee.companyEmail}
-                                                                </td>
-                                                            </tr>
-                                                        ))}
-                                                    </React.Fragment>
-                                                ))}
-                                            </tbody>
-                                        </table>
-                                }
+        <section className="w-full h-full bg-primary-palePastel rounded-tl-3xl rounded-tr-3xl 2xl:rounded-tl-[2.5rem] 2xl:rounded-tr-[2.5rem] px-4 2xl:px-10 pt-12">
+            {addEmployee && <AddEmployeeForm onSetSubmit={onSetSubmit} submit={submit} onSetAddEmployee={onSetAddEmployee} />}
+            <div className="h-full w-full flex flex-col">
+                <h1 className="font-lato font-extrabold text-jetblack text-3xl lg:text-4xl">
+                    Employee Records
+                </h1>
+                <div className="flex h-full overflow-y-hidden pb-14">
+                    <div className="w-full h-full 2xl:w-[70%] py-12">
+                        <div className="flex gap-2">
+                            <div className="relative flex items-center w-full">
+                                <LuSearch size={24} className="absolute ml-3 stroke-gray-400"/>
+                                <input
+                                    placeholder="Search employee"
+                                    className="w-full lg:w-96 rounded-lg p-2.5 pl-10 shadow-md focus:border md:text-lg focus:outline-primary-light"
+                                    />
+                            </div>
+                            <div className="w-1/5 lg:w-56 gap-2 bg-primary-light flex items-center rounded-lg justify-center hover:bg-primary-dark">
+                                <IoMdAdd size={24} className="fill-white" />
+                                <p className="hidden lg:block text-white font-poppins font-semibold">Add Employee</p>
                             </div>
                         </div>
+                        <div className="mt-6 bg-white shadow-lg  h-full overflow-y-auto rounded-xl px-1.5">
+                            <table className="w-full">
+                                <thead className="text-left uppercase self-center sticky top-0 bg-white font-lato mt-8">
+                                    <tr className="">
+                                        <th scope="col" className="text-primary-light py-3 px-6 w-[50%] font-black pt-6">
+                                            Name
+                                        </th> 
+                                        <th scope="col" className="text-primary-light py-3 pt-6  font-black hidden md:table-cell w-[25%]">
+                                            Company Email
+                                        </th> 
+                                        <th scope="col" className="text-primary-light py-3 pt-6 px-6 font-black hidden lg:table-cell xl:pl-6 w-[25%]">
+                                            Mobile Number
+                                        </th> 
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {sortedGroupLetters.map((groupLetter) => (
+                                        <React.Fragment key={groupLetter}>
+                                            <tr className="bg-gray-100 border-none">
+                                                <td colSpan="3" className="px-6 py-1 my-8 text-sm font-semibold">
+                                                    {groupLetter}
+                                                </td>
+                                            </tr>
+                                            {groupedEmployees[groupLetter].map((employee) => (
+                                                <tr
+                                                    key={employees.id}
+                                                    className="hover:bg-primary-pastel cursor-pointer h-full"
+                                                >
+                                                    <td className="px-6 py-3 h-full font-poppins text-gray-600 flex items-center text-ellipsis">
+                                                        <div className="font-medium h-14 w-14 flex justify-center items-center bg-secondary-pastel mr-6 rounded-xl shadow-md">
+                                                            <div className="text-secondary-light text-xl font-poppins">
+                                                                {employee.firstName[0]}{employee.lastName[0]}
+                                                            </div>
+                                                        </div>
+                                                        <div className="flex flex-col text-lg text-gray-800 font-poppins">
+                                                            {employee.firstName}    {employee.lastName}
+                                                            <p className="italic text-xs text-gray-400">
+                                                                { employee.id }
+                                                            </p>
+                                                        </div>
+                                                    </td>
+                                                    <td className="hidden md:table-cell py-2 font-poppins text-gray-500 w-[25%] truncate">
+                                                        {employee.companyEmail}
+                                                    </td>
+                                                    <td className="hidden font-poppins lg:table-cell py-2 lg:pl-6 text-gray-500 w-[25%] truncate">
+                                                        {employee.mobileNumber}
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </React.Fragment>
+                                    ))}
+                                </tbody>
+                                
+                            </table>
+                        </div>
+                    </div>
+                    <div className=" hidden 2xl:block">
+                        <ViewEmployee />
                     </div>
                 </div>
-                <div className="">
-                </div>
             </div>
+
         </ section>
     );
 };
 
 export default EmployeeRecord;
+
