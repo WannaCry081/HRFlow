d8f649f3ab3da0eb3492ba25aa56a915a36f4f3c
Lirae Que Data <liraedata59@gmail.com>
2023-10-15 10:11:42 +0800

    Add join team dto to team dto

---
+++ backend/HRIS/Dtos/TeamDto/JoinTeamDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRIS.Dtos.TeamDto
+{
+    public class JoinTeamDto
+    {
+        [Required(ErrorMessage = "Team code is required.")]
+        [StringLength(8, ErrorMessage = "Team code must be 8 characters long.")]
+        public string Code { get; set; } = string.Empty;
+    }
+}
