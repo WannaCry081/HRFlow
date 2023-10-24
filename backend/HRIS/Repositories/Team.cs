888b4848bd2d9a4ec7388693cc4deb9b80c9b10d
Lirae Que Data <liraedata59@gmail.com>
2023-10-25 00:35:59 +0800

    Update team data annotation value

--- backend/HRIS/Repositories/Team.cs
+++ backend/HRIS/Repositories/Team.cs
@@ -1,8 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text.Json.Serialization;
+using HRIS.Models;
 
-namespace HRIS.Models
+namespace HRIS.Repositories
 {
     [Table("Teams")]
     public class Team
@@ -10,13 +11,16 @@ namespace HRIS.Models
         [Key]
         public Guid Id { get; set; }
 
-        [StringLength(100)]
-        public string Name { get; set; } = String.Empty;
+        [StringLength(50)]
+        public string Name { get; set; } = string.Empty;
 
         [StringLength(8)]
-        public string Code { get; set; } = String.Empty;
+        public string Code { get; set; } = string.Empty;
 
         [JsonIgnore]
         public ICollection<User> Users { get; set; } = new List<User>();
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
     }
 }
