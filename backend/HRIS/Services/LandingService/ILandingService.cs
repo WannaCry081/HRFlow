27baca2498b23612031c8b5fa6dcf774bccb4d1d
Lirae Que Data <liraedata59@gmail.com>
2023-10-25 21:58:40 +0800

    Add new ContactUs implementation to landing

--- backend/HRIS/Services/LandingService/ILandingService.cs
+++ backend/HRIS/Services/LandingService/ILandingService.cs
@@ -1,6 +1,9 @@
-﻿namespace HRIS.Services.LandingService
+﻿using HRIS.Dtos.LandingDto;
+
+namespace HRIS.Services.LandingService
 {
     public interface ILandingService
     {
+        Task<string> ContactUs(ContactUsDto request);
     }
 }
--- backend/HRIS/Services/LandingService/LandingService.cs
+++ backend/HRIS/Services/LandingService/LandingService.cs
@@ -1,6 +1,21 @@
-﻿namespace HRIS.Services.LandingService
+﻿using HRIS.Dtos.LandingDto;
+using HRIS.Utils;
+
+namespace HRIS.Services.LandingService
 {
     public class LandingService : ILandingService
     {
+        private readonly IConfiguration _configuration;
+        public LandingService(IConfiguration configuration)
+        {
+            _configuration = configuration ??
+                throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public async Task<string> ContactUs(ContactUsDto request)
+        {
+            return await SMTP.SendEmailToPage(
+                _configuration, request.Email, request.Subject, request.Body);
+        }
     }
 }
