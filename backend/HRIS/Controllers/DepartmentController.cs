3453ab8f96e33699e3ba54e219ed07f41fd6551a
Lirae Que Data <liraedata59@gmail.com>
2023-10-26 13:30:56 +0800

    Add new department controller

---
+++ backend/HRIS/Controllers/DepartmentController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRIS.Controllers
+{
+    [Authorize(Roles = "/api/department")]
+    [ApiController]
+    [Route("/api/department")]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly ILogger<DepartmentController> _logger;
+
+        public DepartmentController(ILogger<DepartmentController> logger)
+        {
+            _logger = logger ??
+                throw new ArgumentNullException(nameof(logger));
+        }
+    }
+}
