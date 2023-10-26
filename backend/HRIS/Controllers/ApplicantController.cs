65f26b060238a97aefc46a8a43aa59fe63ba66c7
Lirae Que Data <liraedata59@gmail.com>
2023-10-26 13:30:34 +0800

    Add new applicant controller

---
+++ backend/HRIS/Controllers/ApplicantController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRIS.Controllers
+{
+    [Authorize(Roles = "Human Resource")]
+    [ApiController]
+    [Route("/api/applicant")]
+    public class ApplicantController : ControllerBase
+    {
+        private readonly ILogger<ApplicantController> _logger;
+
+        public ApplicantController(ILogger<ApplicantController> logger)
+        {
+            _logger = logger ??
+                throw new ArgumentNullException(nameof(logger));
+        }
+    }
+}
