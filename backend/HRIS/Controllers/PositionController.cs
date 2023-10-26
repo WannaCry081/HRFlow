e22b242105fec4a8a30895ab734eb3bfce780622
Lirae Que Data <liraedata59@gmail.com>
2023-10-26 13:31:10 +0800

    Add new position controller

---
+++ backend/HRIS/Controllers/PositionController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRIS.Controllers
+{
+    [Authorize(Roles = "Human Resource")]
+    [ApiController]
+    [Route("/api/position")]
+    public class PositionController : ControllerBase
+    {
+        private readonly ILogger<PositionController> _logger;
+
+        public PositionController(ILogger<PositionController> logger)
+        {
+            _logger = logger ??
+                throw new ArgumentNullException(nameof(logger));
+        }
+    }
+}
