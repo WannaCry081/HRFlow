03803db829296232f3a86727218e7386accd6bff
Lirae Que Data <liraedata59@gmail.com>
2023-10-26 13:31:25 +0800

    Add new record controller

---
+++ backend/HRIS/Controllers/RecordController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HRIS.Controllers
+{
+    [Authorize(Roles = "Human Resource")]
+    [ApiController]
+    [Route("/api/record")]
+    public class RecordController : ControllerBase
+    {
+        private readonly ILogger<RecordController> _logger;
+
+        public RecordController(ILogger<RecordController> logger)
+        {
+            _logger = logger ?? 
+                throw new ArgumentNullException(nameof(logger));
+        }
+    }
+}
