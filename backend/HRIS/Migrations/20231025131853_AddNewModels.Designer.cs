42e896bcd21f0201acb554725e94d41c2b317599
Lirae Que Data <liraedata59@gmail.com>
2023-10-26 14:17:15 +0800

    Add new migrations

--- backend/HRIS/Migrations/20231025131853_AddNewModels.Designer.cs
+++ backend/HRIS/Migrations/20231025131853_AddNewModels.Designer.cs
@@ -4,6 +4,7 @@ using HRIS.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HRIS.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20231025131853_AddNewModels")]
+    partial class AddNewModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
