7b54d266398360a1f97d830b8222c734c56d10f7
shielamae02 <shiela.mlepon@gmail.com>
2023-10-31 22:58:01 +0800

    Add department repository logic

--- backend/HRIS/Repositories/DepartmentRepository/DepartmentRepository.cs
+++ backend/HRIS/Repositories/DepartmentRepository/DepartmentRepository.cs
@@ -1,6 +1,58 @@
-﻿namespace HRIS.Repositories.DepartmentRepository
+﻿using HRIS.Context;
+using HRIS.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection.Metadata.Ecma335;
+
+namespace HRIS.Repositories.DepartmentRepository
 {
     public class DepartmentRepository : IDepartmentRepository
     {
+        private readonly DataContext _context;
+
+        public DepartmentRepository(DataContext context)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        public async Task<bool> IsDepartmentExists(string name)
+        {
+            return await _context.Departments.Where(
+                c => c.Name.Equals(name)).AnyAsync();
+        }
+
+        public async Task<User?> GetUserById(Guid id)
+        {
+            return await _context.Users.Where(
+                c => c.Id.Equals(id)).FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> CreateDepartment(Department department)
+        {
+            _context.Departments.Add(department);
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public async Task<Department?> GetDepartment(User hr, Guid departmentId)
+        {
+            return await _context.Departments.Where(
+                c => c.Id.Equals(departmentId) && c.TeamId.Equals(hr.TeamId)).FirstOrDefaultAsync();
+        }
+
+        public async Task<ICollection<Department>> GetDepartments(User hr)
+        {
+            return await _context.Departments.Where(
+                c => c.TeamId.Equals(hr.TeamId)).ToListAsync(); 
+        }
+
+        public Task<bool> UpdateDepartment(Department department, JsonPatchDocument<Department> request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateDepartments(Department department, Department request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
