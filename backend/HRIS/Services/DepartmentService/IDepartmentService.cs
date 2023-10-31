a971651a66c4f47b0737905a339adf1649d35297
shielamae02 <shiela.mlepon@gmail.com>
2023-10-31 23:55:18 +0800

    Add department service interface

--- backend/HRIS/Services/DepartmentService/IDepartmentService.cs
+++ backend/HRIS/Services/DepartmentService/IDepartmentService.cs
@@ -1,6 +1,15 @@
-﻿namespace HRIS.Services.DepartmentService
+﻿using HRIS.Dtos.DepartmentDto;
+using HRIS.Models;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace HRIS.Services.DepartmentService
 {
     public interface IDepartmentService
     {
+        Task<GetDepartmentDto> GetDepartment(Guid hrId, Guid departmentId);
+        Task<ICollection<GetDepartmentDto>> GetDepartments(Guid hrId);
+        Task<GetDepartmentDto> CreateDepartment(Guid hrId, CreateDepartmentDto request);
+        Task<GetDepartmentDto> UpdateDepartments(Guid hrId, Guid departmentId, UpdateDepartmentDto request);
+        Task<bool> UpdateDepartment(Guid hrId, Guid departmentId, JsonPatchDocument<Department> request);
     }
 }
