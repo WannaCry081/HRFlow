8e9bad756944ff01b45410a5426cef1c955d10b3
shielamae02 <shiela.mlepon@gmail.com>
2023-10-31 16:39:30 +0800

    Add department repository interface

--- backend/HRIS/Repositories/DepartmentRepository/IDepartmentRepository.cs
+++ backend/HRIS/Repositories/DepartmentRepository/IDepartmentRepository.cs
@@ -1,6 +1,15 @@
-﻿namespace HRIS.Repositories.DepartmentRepository
+﻿using HRIS.Models;
+using Microsoft.AspNetCore.JsonPatch;
+
+namespace HRIS.Repositories.DepartmentRepository
 {
     public interface IDepartmentRepository
     {
+        Task<Department?> GetDepartment(User hr, Guid departmentId);
+        Task<ICollection<Department>> GetDepartments(User hr);
+        Task<bool> CreateDepartment(Department department);
+        Task<bool> UpdateDepartments(Department department, Department request);
+        Task<bool> UpdateDepartment(Department department, JsonPatchDocument<Department> request);
+
     }
 }
