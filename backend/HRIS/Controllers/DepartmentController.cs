0db9dcf3e13c56587535595414c25ff42d281e29
shielamae02 <shiela.mlepon@gmail.com>
2023-10-31 23:53:48 +0800

    Add department controller logic

--- backend/HRIS/Controllers/DepartmentController.cs
+++ backend/HRIS/Controllers/DepartmentController.cs
@@ -1,49 +1,159 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using HRIS.Dtos.DepartmentDto;
+using HRIS.Exceptions;
+using HRIS.Models;
+using HRIS.Services.DepartmentService;
+using HRIS.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HRIS.Controllers
 {
-    [Authorize(Roles = "/api/department")]
+    [Authorize(Roles = "Human Resource")]
     [ApiController]
     [Route("/api/department")]
     public class DepartmentController : ControllerBase
     {
         private readonly ILogger<DepartmentController> _logger;
+        private readonly IDepartmentService _departmentService;
 
-        public DepartmentController(ILogger<DepartmentController> logger)
+        public DepartmentController(ILogger<DepartmentController> logger, IDepartmentService departmentService)
         {
             _logger = logger ??
                 throw new ArgumentNullException(nameof(logger));
+            _departmentService = departmentService ??
+                throw new ArgumentNullException(nameof(departmentService));
         }
 
         [HttpGet]
-        public Task<IActionResult> GetDepartments()
+        [Produces("application/json")]
+        public async Task<IActionResult> GetDepartments()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var hrId = UserClaim.GetCurrentUser(HttpContext) ??
+                    throw new UserNotFoundException("Invalid user's credential. Please try again.");
+                var response = await _departmentService.GetDepartments(hrId);
+                return Ok(response);
+            }
+            catch (UserNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to find employee.", ex);
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while attempting to get department.");
+                return Problem("Internal server error.");
+            }
         }
 
         [HttpGet("{departmentId}")]
-        public Task<IActionResult> GetDepartment([FromRoute] Guid departmentId)
+        [Produces("application/json")]
+        public async Task<IActionResult> GetDepartment([FromRoute] Guid departmentId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var hrId = UserClaim.GetCurrentUser(HttpContext) ??
+                    throw new UserNotFoundException("Invalid user's credential. Please try again.");
+                var response = await _departmentService.GetDepartment(hrId, departmentId);
+                return Ok(response);
+            }
+            catch (UserNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to find employee.", ex);
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while attempting to get departments.");
+                return Problem("Internal server error.");
+            }
         }
 
-        [HttpPost] 
-        public Task<IActionResult> CreateDepartment()
+        [HttpPost]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public async Task<IActionResult> CreateDepartment([FromBody] CreateDepartmentDto request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var hrId = UserClaim.GetCurrentUser(HttpContext) ??
+                    throw new UserNotFoundException("Invalid user's credential. Please try again.");
+                var response = await _departmentService.CreateDepartment(hrId, request);
+                return Ok(response);
+            }
+            catch (UserNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to find employee.", ex);
+                return NotFound(ex.Message);
+            }
+            catch (DepartmentExistsException ex)
+            {
+                _logger.LogError("An error occurred while attempting to add a department to the database.", ex);
+                return BadRequest(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while attempting to add a new department.");
+                return Problem("Internal server error.");
+            }
         }
 
-        [HttpPut("{departmentId}")] 
-        public Task<IActionResult> UpdateDepartment([FromRoute] Guid departmentId)
+        [HttpPatch("{departmentId}")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+
+        public async Task<IActionResult> UpdateDepartment([FromRoute] Guid departmentId, [FromBody] JsonPatchDocument<Department> request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var hrId = UserClaim.GetCurrentUser(HttpContext) ??
+                    throw new UserNotFoundException("Invalid user's credential. Please try again.");
+                var response = await _departmentService.UpdateDepartment(hrId, departmentId, request);
+                return Ok("Successfully updated department's information.");
+            }
+            catch (UserNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to find employee.", ex);
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while attempting to update department information.");
+                return Problem("Internal server error.");
+            }
+
         }
 
-        [HttpDelete("{departmentId}")] 
+        [HttpPut("{departmentId}")]
+        [Consumes("application/json")]
+        [Produces("application/json")]
+        public async Task<IActionResult> UpdateDepartments([FromRoute] Guid departmentId, [FromBody] UpdateDepartmentDto request)
+        {
+            try
+            {
+                var hrId = UserClaim.GetCurrentUser(HttpContext) ??
+                    throw new UserNotFoundException("Invalid user's credential. Please try again.");
+                var response = await _departmentService.UpdateDepartments(hrId, departmentId, request);
+                return Ok(response);
+            }
+            catch (UserNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to find employee.", ex);
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while attempting to update department information.");
+                return Problem("Internal server error.");
+            }
+        }
+
+        [HttpDelete("{departmentId}")]
         public Task<IActionResult> DeleteDepartment([FromRoute] Guid departmentId)
         {
             throw new NotImplementedException();
-        } 
+        }
     }
 }
