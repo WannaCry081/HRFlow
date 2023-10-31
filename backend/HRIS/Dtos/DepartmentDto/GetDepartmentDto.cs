6377bde3f21fbade4c70bd1f387132350c300e60
shielamae02 <shiela.mlepon@gmail.com>
2023-10-31 22:59:02 +0800

    Add get department dto

---
+++ backend/HRIS/Dtos/DepartmentDto/GetDepartmentDto.cs
@@ -0,0 +1,15 @@
+﻿using HRIS.Models;
+
+namespace HRIS.Dtos.DepartmentDto
+{
+    public class GetDepartmentDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Manager { get; set; } = string.Empty;
+        public string Assistant { get; set; } = string.Empty;
+        public ICollection<User> Supervisors { get; set; } = new List<User>();
+        public Guid? TeamId { get; set; }
+        public ICollection<Position> Positions { get; set; } = new List<Position>();
+    }
+}
