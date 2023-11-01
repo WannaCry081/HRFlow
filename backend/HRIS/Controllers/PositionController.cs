c152ece01ce5da53dbd9ef1a12c0c3c1ce0c0541
shielamae02 <shiela.mlepon@gmail.com>
2023-11-01 18:54:04 +0800

    Add get positions method in Position Controller

--- backend/HRIS/Controllers/PositionController.cs
+++ backend/HRIS/Controllers/PositionController.cs
@@ -1,25 +1,56 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using HRIS.Dtos.PositionDto;
+using HRIS.Exceptions;
+using HRIS.Models;
+using HRIS.Services.PositionService;
+using HRIS.Utils;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HRIS.Controllers
 {
     [Authorize(Roles = "Human Resource")]
     [ApiController]
-    [Route("/api/position")]
+    [Route("/api/department/{departmentId}/position")]
     public class PositionController : ControllerBase
     {
         private readonly ILogger<PositionController> _logger;
+        private readonly IPositionService _positionService;
 
-        public PositionController(ILogger<PositionController> logger)
+        public PositionController(ILogger<PositionController> logger, IPositionService positionService)
         {
             _logger = logger ??
                 throw new ArgumentNullException(nameof(logger));
+            _positionService = positionService ??
+                throw new ArgumentException(nameof(positionService));
         }
 
         [HttpGet]
-        public Task<IActionResult> GetPositions()
+        [Produces("application/json")]
+        public async Task<IActionResult> GetPositions([FromRoute] Guid departmentId)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var hrId = UserClaim.GetCurrentUser(HttpContext) ??
+                    throw new UserNotFoundException("Invalid user's credential. Please try again.");
+                var response = await _positionService.GetPositions(hrId, departmentId);
+                return Ok(response);
+        }
+            catch (UserNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to find employee.", ex);
+                return NotFound(ex.Message);
+            }
+            catch (DepartmentNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to find department.", ex);
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while attempting to get positions.");
+                return Problem("Internal server error.");
+            }
         }
 
         [HttpGet("{positionId}")]
