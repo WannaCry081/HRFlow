8b13bd60a8b5ed52aeca085af3523a6e8b4a4522
shielamae02 <shiela.mlepon@gmail.com>
2023-11-01 19:01:44 +0800

    Add create position dto

---
+++ backend/HRIS/Dtos/PositionDto/CreatePositionDto.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HRIS.Dtos.PositionDto
+{
+    public class CreatePositionDto
+    {
+        [Required(ErrorMessage = "Title is required.")]
+        [StringLength(50, ErrorMessage = "Title must be between {2} and {1} characters long.", MinimumLength = 2)]
+        public string Title { get; set; } = string.Empty;
+    }
+}
