8f727a88896eb02c599bd9f91447bf663dc556e5
shielamae02 <shiela.mlepon@gmail.com>
2023-11-01 18:59:09 +0800

    Add update department dto

--- backend/HRIS/Dtos/DepartmentDto/UpdateDepartmentDto.cs
+++ backend/HRIS/Dtos/DepartmentDto/UpdateDepartmentDto.cs
@@ -4,8 +4,8 @@ namespace HRIS.Dtos.DepartmentDto
 {
     public class UpdateDepartmentDto
     {
-        [Required(ErrorMessage = "Department Name is required.")]
-        [StringLength(50, ErrorMessage = "Department Name must be between {2} and {1} characters long.", MinimumLength = 2)]
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(50, ErrorMessage = "Name must be between {2} and {1} characters long.", MinimumLength = 2)]
         public string Name { get; set; } = string.Empty;
 
         [Required(ErrorMessage = "Manager is required.")]
