16b80b04346f38953707035d6007eea20cafcc73
Lirae Que Data <liraedata59@gmail.com>
2023-11-04 18:21:46 +0800

    Add applicant mapper configuration

---
+++ backend/HRIS/Mappers/ApplicantMapper.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using HRIS.Models;
+using HRIS.Dtos.ApplicantDto;
+
+namespace HRIS.Mappers
+{
+    public class ApplicantMapper : Profile
+    {
+        public ApplicantMapper()
+        {
+            CreateMap<Applicant, GetApplicantDto>().ReverseMap();
+        }
+    }
+}
