cb93e43baa6ec307aa72547d3bbd73a740846ea0
Gerald Carido <geraldcastcarido@gmail.com>
2023-11-04 10:52:42 +0800

    Add UpdateRecordDto

---
+++ backend/HRIS/dtos/RecordDto/UpdateRecordDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRIS.Dtos
+{
+    public class UpdateRecordDto
+    {
+
+        [Required]
+        public DateTime ClockOut { get; set; } = DateTime.Now;
+    }
+}
