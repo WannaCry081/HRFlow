9a999077f545230081b0d761e6b0ae63f3c25e1f
Lirae Que Data <liraedata59@gmail.com>
2023-11-04 18:23:03 +0800

    Add new get applicant records in applicant service

--- backend/HRIS/Services/ApplicantService/ApplicantService.cs
+++ backend/HRIS/Services/ApplicantService/ApplicantService.cs
@@ -1,6 +1,28 @@
-﻿namespace HRIS.Services.ApplicantService
+﻿using AutoMapper;
+using HRIS.Dtos.ApplicantDto;
+using HRIS.Exceptions;
+using HRIS.Repositories.ApplicantRepository;
+
+namespace HRIS.Services.ApplicantService
 {
     public class ApplicantService : IApplicantService
     {
+        private readonly IMapper _mapper;
+        private readonly IApplicantRepository _applicantRepository;
+
+        public ApplicantService(IMapper mapper, IApplicantRepository applicantRepository)
+        {
+            _mapper = mapper;
+            _applicantRepository = applicantRepository;
+        }
+
+        public async Task<ICollection<GetApplicantDto>> GetApplicantRecords(Guid hrId)
+        {
+            var hr = await _applicantRepository.GetUserById(hrId) ??
+                throw new UserNotFoundException("Invalid email address. Please try again.");
+
+            var response = await _applicantRepository.GetApplicantRecords(hr);
+            return _mapper.Map<ICollection<GetApplicantDto>>(response);
+        }
     }
 }
--- backend/HRIS/Services/ApplicantService/IApplicantService.cs
+++ backend/HRIS/Services/ApplicantService/IApplicantService.cs
@@ -1,6 +1,9 @@
-﻿namespace HRIS.Services.ApplicantService
+﻿using HRIS.Dtos.ApplicantDto;
+
+namespace HRIS.Services.ApplicantService
 {
     public interface IApplicantService
     {
+        Task<ICollection<GetApplicantDto>> GetApplicantRecords(Guid hrId);
     }
 }
