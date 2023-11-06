2a45dd0041220453025ed7f78cffb8e8e5b1a1ab
Gerald Carido <geraldcastcarido@gmail.com>
2023-11-06 19:12:36 +0800

    Add route url for record

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -5,3 +5,4 @@ export const AUTH_URL = BASE_URL + "/auth";
 export const TEAM_URL = BASE_URL + "/team";
 export const EMPLOYEE_URL = BASE_URL + "/employee";
 export const USER_URL = BASE_URL + "/user";
+export const RECORD_URL = BASE_URL + "/record";
