265c48ee25d2e3f3f8be852508599f9412525b44
Lirae Que Data <liraedata59@gmail.com>
2023-11-08 01:21:58 +0800

    Update property value to vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,37 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve : {
-    alias : [{
-      find : "@",
-      replacement : "/src"
-    }, {
-      find : "@Components",
-      replacement : "/src/components"
-    }, {
-      find : "@Pages",
-      replacement : "/src/pages",
-    }, {
-      find : "@Services",
-      replacement : "/src/services",
-    }, {
-      find : "@Utils",
-      replacement : "/src/Utils",
-    }, {
-      find : "@Assets",
-      replacement : "/src/assets",
-    }, {
-      find : "@Layouts",
-      replacement : "/src/layouts",
-    }, {
-      find : "@Styles",
-      replacement : "/src/styles",
-    }, {
-      find : "@Hooks",
-      replacement : "/src/hooks",
-    }]
-  }
-})
+  resolve: {
+    alias: [
+      {
+        find: "@Src",
+        replacement: "/src",
+      },
+      {
+        find: "@Components",
+        replacement: "/src/components",
+      },
+      {
+        find: "@Pages",
+        replacement: "/src/pages",
+      },
+      {
+        find: "@Services",
+        replacement: "/src/services",
+      },
+      {
+        find: "@Utils",
+        replacement: "/src/Utils",
+      },
+      {
+        find: "@Assets",
+        replacement: "/src/assets",
+      },
+      {
+        find: "@Layouts",
+        replacement: "/src/layouts",
+      },
+      {
+        find: "@Styles",
+        replacement: "/src/styles",
+      },
+      {
+        find: "@Hooks",
+        replacement: "/src/hooks",
+      },
+    ],
+  },
+});
