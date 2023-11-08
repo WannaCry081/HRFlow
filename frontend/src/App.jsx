65a3c98a5085e1733f0939624753b15303895989
Lirae Que Data <liraedata59@gmail.com>
2023-11-08 22:13:07 +0800

    Add configuration to private route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,13 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import Auth from "@Layouts/Auth";
 import Dashboard from "@Layouts/Dashboard";
 import Landing from "@Pages/Landing"; 
+import PrivateRoute from "@Utils/PrivateRoute";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" index element={<Landing />} />
       <Route path="/auth/*" element={<Auth />} /> 
-      <Route path="/dashboard/*" element={<Dashboard />} />
+      <Route path="/dashboard/*" element={
+        <PrivateRoute element={<Dashboard />} />
+      } />
       <Route path="/error" element={<h1>Internal Server Error</h1>} />
     </Routes>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import App from "@/App";
+import App from "@Src/app";
 import "@Styles/main.css";
 
-const rootElement = document.querySelector("#root");
-
-const root = ReactDOM.createRoot(rootElement);
+const root = ReactDOM.createRoot(
+  document.querySelector("#root"));
 
 root.render(
   <React.StrictMode>
