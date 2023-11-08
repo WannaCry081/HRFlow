ca4059f58ba838b892ec30fd0e0b73bc7fe3bfbb
Lirae Que Data <liraedata59@gmail.com>
2023-11-08 22:11:10 +0800

    Add new private route

--- frontend/src/utils/PrivateRoute.jsx
+++ frontend/src/utils/PrivateRoute.jsx
@@ -1,8 +1,16 @@
-const PrivateRoute = () => {
-    return (
-        <>
-        </>
-    );
+import { useState, useEffect } from "react";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({ element }) => {
+    
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+    useEffect(() => {
+        const token = sessionStorage.getItem("token");
+        setIsAuthenticated(!!token);
+    }, []);
+
+    return isAuthenticated ? element : <Navigate to="/dashboard/home" />;
 };
 
 export default PrivateRoute;
