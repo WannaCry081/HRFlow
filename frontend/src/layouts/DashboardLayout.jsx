47ceca048d3c6141385e167189e09b93678bae45
Lirae Que Data <liraedata59@gmail.com>
2023-11-09 15:50:05 +0800

    Add new dashboard layout

--- frontend/src/layouts/DashboardLayout.jsx
+++ frontend/src/layouts/DashboardLayout.jsx
@@ -1,57 +1,55 @@
 import { useEffect, useState } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
-import { routes } from "@Utils/Routers";
+import { Routers } from "@Utils/Routers";
 import NavBar from "@Components/NavBar";
-import { useNavigate } from "react-router-dom";
 import { GetUserProfileApi } from "@Services/userService.js";
 import Header from "@Components/Header";
 
-const Dashboard = () => {
-    document.title = "HR Flow | Dashboard";
+const DashboardLayout = () => {
+  const [userData, setUserData] = useState({});
 
-    const navigate = useNavigate();
-    const [userData, setUserData ] = useState({});
+  useEffect(() => {
+    const token = sessionStorage.getItem("token");
 
-    useEffect(() => {
-        const token = sessionStorage.getItem("token");  
-        if (token === undefined || token === null) {
-            navigate("/auth/login", { replace : true });
-        }
+    const getUserProfile = async () => {
+      const { status, data } = await GetUserProfileApi(token);
+      setUserData(data);
+    };
 
-        const getUserProfile = async () => {
-            const {status, data} = await GetUserProfileApi(token);
-            setUserData(data);
-        };
+    getUserProfile();
+  }, []);
 
-        getUserProfile();
-    }, []);
-
-    return (
-        <main className="h-screen w-screen flex">
-            <NavBar routes={routes} userData={userData}/>
-            <section className="w-full flex flex-col bg-gray-50">
-                <Header />
-                <div className="flex-grow ">
-                    <Routes>
-                        <Route path="*" element={<Navigate to="/dashboard/home" replace/>} />
-                        {routes.map(({ role, layout, pages }) =>
-                            layout === "Dashboard" && 
-                            role === userData.role &&
-                            pages.map(({ path, element }) => (
-                                <Route exact path={path} element={element} />
-                            ))
-                        )}
-                        {routes.map(({ layout, pages }) => 
-                            layout === "Common" && 
-                            pages.map(({ path, element }) => (
-                                <Route exact path={path} element={element} />
-                            ))
-                        )}
-                    </Routes>
-                </div>
-            </section>
-        </main>
-    );
+  return (
+    <main className="h-screen w-screen flex">
+      <NavBar routes={Routers} userData={userData} />
+      <section className="w-full flex flex-col bg-gray-50">
+        <Header />
+        <div className="flex-grow ">
+          <Routes>
+            <Route
+              path="*"
+              element={<Navigate to="/dashboard/home" replace />}
+            />
+            {Routers.map(
+              ({ role, layout, pages }) =>
+                layout === "Dashboard" &&
+                role === userData.role &&
+                pages.map(({ path, element }) => (
+                  <Route exact path={path} element={element} />
+                ))
+            )}
+            {Routers.map(
+              ({ layout, pages }) =>
+                layout === "Common" &&
+                pages.map(({ path, element }) => (
+                  <Route exact path={path} element={element} />
+                ))
+            )}
+          </Routes>
+        </div>
+      </section>
+    </main>
+  );
 };
 
-export default Dashboard;
+export default DashboardLayout;
