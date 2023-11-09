efd1782da3cc4af89076233bf096ede5883aa19f
Lirae Que Data <liraedata59@gmail.com>
2023-11-09 15:49:46 +0800

    Add new landing layout

--- frontend/src/layouts/LandingLayout.jsx
+++ frontend/src/layouts/LandingLayout.jsx
@@ -1,8 +1,21 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Routers } from "@Utils/Routers";
+
 const LandingLayout = () => {
-    return (
-        <>
-        </>
-    );
+  return (
+    <>
+      <Routes>
+        <Route path="*" element={<Navigate to="/" />} />
+        {Routers.map(
+          ({ layout, pages }) =>
+            layout === "Landing" &&
+            pages.map(({ path, element }) => (
+              <Route path={path} element={element} />
+            ))
+        )}
+      </Routes>
+    </>
+  );
 };
 
 export default LandingLayout;
