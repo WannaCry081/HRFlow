02ae3a40f854fe9544ab94d03a1ba953358f9873
shielamae02 <shiela.mlepon@gmail.com>
2023-11-10 11:04:06 +0800

    Add parent layout

--- frontend/src/layouts/LandingLayout.jsx
+++ frontend/src/layouts/LandingLayout.jsx
@@ -1,20 +1,43 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Routers } from "@Utils/Routers";
+import { useRef } from "react";
+import  Navbar from "@Pages/Landing/components/Navbar";
 
 const LandingLayout = () => {
+  document.title = "HR Flow | Streamline HR tasks";
+
+  const sectionRefs = {
+    homeRef: useRef(null),
+    servicesRef: useRef(null),
+    aboutRef: useRef(null),
+    contactRef: useRef(null),
+  };
+
+  const menuItemClickHandler = (ref) => {
+    ref.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
-    <>
-      <Routes>
-        <Route path="*" element={<Navigate to="/" />} />
-        {Routers.map(
-          ({ layout, pages }) =>
-            layout === "Landing" &&
-            pages.map(({ path, element }) => (
-              <Route path={path} element={element} />
-            ))
-        )}
-      </Routes>
-    </>
+    <main className="relative w-screen overflow-x-hidden">
+      <div className="absolute top-0 left-0 right-0">
+        <Navbar
+          scrollIntoView={menuItemClickHandler}
+          sectionRefs={sectionRefs}
+        />
+      </div>
+      <div className="h-screen px-4">
+        <Routes>
+          <Route path="*" element={<Navigate to="/" />} />
+          {Routers.map(
+            ({ layout, pages }) =>
+              layout === "Landing" &&
+              pages.map(({ path, element }) => (
+                <Route path={path} element={element} />
+              ))
+          )}
+        </Routes>
+      </div>
+    </main>
   );
 };
 
