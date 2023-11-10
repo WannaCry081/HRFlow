b15eb2504fe99ff8dcb30bd537f84ea15239fdc4
shielamae02 <shiela.mlepon@gmail.com>
2023-11-10 11:03:21 +0800

    add useEmployee hook

---
+++ frontend/src/hooks/useEmployees.jsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+import { GetEmployeesApi } from "@Services/employeeService";
+
+const useEmployees = (submit) => {
+
+    const token = sessionStorage.getItem("token");
+    const [employees, setEmployees] = useState([]);
+
+    useEffect(() => {
+        const getEmployees = async () => {
+            const response = await GetEmployeesApi(token);
+            setEmployees(response.data);
+        }
+
+        getEmployees();
+    }, [submit])
+
+    return employees;
+
+}
+export default useEmployees;
