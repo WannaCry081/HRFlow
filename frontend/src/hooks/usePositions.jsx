db876210bc59a60ac4c6b2426a08040a25b763d1
shielamae02 <shiela.mlepon@gmail.com>
2023-11-10 11:03:46 +0800

    Add usePositions hook

---
+++ frontend/src/hooks/usePositions.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from "react";
+import { GetPositionsApi } from "@Services/positionService"
+
+const usePositions = (submit, departmentId) => {
+
+    const token = sessionStorage.getItem("token");
+
+    const [positions, setPositions] = useState([]);
+
+    useEffect(() => {
+
+        const getPositions = async () => {
+            if(departmentId){
+                const response = await GetPositionsApi(token, departmentId);
+                setPositions(response.data);
+            } else {
+                setPositions([]);
+            }
+        };
+
+        getPositions();
+    }, [submit]);
+
+    return positions;
+
+}   
+
+export default usePositions;
