383ba9191b29bdb4daf7ff98faee1ab34136fbff
Lirae Que Data <liraedata59@gmail.com>
2023-11-18 15:53:45 +0800

    Add new notification controller

---
+++ backend/HRIS/Controllers/NotificationController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRIS.Controllers
+{
+    [ApiController]
+    [Route("/api/notification")]
+    public class NotificationController : ControllerBase
+    {
+    }
+}
