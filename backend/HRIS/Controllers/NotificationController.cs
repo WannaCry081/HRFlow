2075cf2d76ec4793e3ac57292d4797f71c7c4d3b
Lirae Que Data <liraedata59@gmail.com>
2023-11-18 16:26:28 +0800

    Add methods to notification controllers

--- backend/HRIS/Controllers/NotificationController.cs
+++ backend/HRIS/Controllers/NotificationController.cs
@@ -1,10 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HRIS.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("/api/notification")]
     public class NotificationController : ControllerBase
     {
+        [HttpGet]
+        public Task<IResult> GetNotifications()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet("{notificationId}")]
+        public Task<IResult> GetNotification([FromRoute] Guid notificationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Human Resource")]
+        [HttpPost]
+        public Task<IResult> CreateNotification()
+        {
+            throw new NotImplementedException();
+        }
+
+        [Authorize(Roles = "Human Resource")]
+        [HttpPut("{notificationId}")]
+        public Task<IResult> UpdateNotification([FromRoute] Guid notificationId)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpDelete("{notificationId}")]
+        public Task<IResult> DeleteNotification([FromRoute] Guid notificationId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
