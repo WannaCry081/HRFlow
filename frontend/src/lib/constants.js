bd0288503aef0fb87de0ee8426d02f4617a915e7
shielamae02 <shiela.mlepon@gmail.com>
2023-11-23 01:05:40 +0800

    Update method and response message

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -1,5 +1,5 @@
-// const BASE_URL = "https://localhost:55534/api";
-const BASE_URL = "http://localhost:8000/api";
+const BASE_URL = "https://localhost:55534/api";
+// const BASE_URL = "http://localhost:8000/api";
 
 export const AUTH_URL = BASE_URL + "/auth";
 export const TEAM_URL = BASE_URL + "/team";
