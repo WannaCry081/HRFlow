ca64d1474dd779d6843c0f1f58172a7df126eb23
shielamae02 <shiela.mlepon@gmail.com>
2023-11-23 00:21:21 +0800

    Add get notifications controller logic

--- backend/HRIS/Controllers/NotificationController.cs
+++ backend/HRIS/Controllers/NotificationController.cs
@@ -8,10 +8,38 @@ namespace HRIS.Controllers
     [Route("/api/notification")]
     public class NotificationController : ControllerBase
     {
-        [HttpGet]
-        public Task<IResult> GetNotifications()
+        private readonly ILogger<NotificationController> _logger;
+        private readonly INotificationService _notificationService;
+
+        public NotificationController(ILogger<NotificationController> logger, INotificationService notificationService)
         {
-            throw new NotImplementedException();
+            _logger = logger ??
+                throw new ArgumentNullException(nameof(logger));
+            _notificationService = notificationService ??
+                throw new ArgumentNullException(nameof(notificationService));
+        }
+
+        [HttpGet]
+        [Produces("application/json")]
+        public async Task<IActionResult> GetNotifications()
+        {
+            try
+            {
+                var hrId = UserClaim.GetCurrentUser(HttpContext) ??
+                    throw new UserNotFoundException("Invalid user's credential. Please try again.");
+                var response = await _notificationService.GetNotifications(hrId);
+                return Ok(response);
+        }
+            catch (UserNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to find employee.", ex);
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while attempting to get positions.");
+                return Problem("Internal server error.");
+            }
         }
 
         [HttpGet("{notificationId}")]
