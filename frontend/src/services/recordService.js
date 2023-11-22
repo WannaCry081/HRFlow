f24985cb3db7e81958658cfbec448f6d8efdb55d
Gerald Carido <geraldcastcarido@gmail.com>
2023-11-22 10:17:37 +0800

    Fix record service directory

--- frontend/src/services/recordService.js
+++ frontend/src/services/recordService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { RECORD_URL } from "@Utils/constants.js";
+import { RECORD_URL } from "/src/lib/constants.js";
 
 const axiosInstance = (token) => axios.create({
   baseURL : RECORD_URL,
