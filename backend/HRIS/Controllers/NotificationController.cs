43e46a6f369c1bbad742409ef384aeb368832984
shielamae02 <shiela.mlepon@gmail.com>
2023-11-23 00:30:00 +0800

    Add update notification property controller logic

--- backend/HRIS/Controllers/NotificationController.cs
+++ backend/HRIS/Controllers/NotificationController.cs
@@ -4,6 +4,7 @@ using HRIS.Models;
 using HRIS.Services.NotificationService;
 using HRIS.Utils;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HRIS.Controllers
@@ -102,9 +103,33 @@ namespace HRIS.Controllers
 
         [Authorize(Roles = "Human Resource")]
         [HttpPut("{notificationId}")]
-        public Task<IResult> UpdateNotification([FromRoute] Guid notificationId)
+        [Consumes("application/json")]
+        [Produces("application/json")]
+
+        public async Task<IActionResult> UpdateNotification([FromRoute] Guid notificationId, [FromBody] JsonPatchDocument<Notification> request)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var hrId = UserClaim.GetCurrentUser(HttpContext) ??
+                    throw new UserNotFoundException("Invalid user's credential. Please try again.");
+                var response = await _notificationService.UpdateNotification(hrId, notificationId, request);
+                return Ok(response);
+            }
+            catch (UserNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to find employee.", ex);
+                return NotFound(ex.Message);
+            }
+            catch (NotificationNotFoundException ex)
+            {
+                _logger.LogError("An error occurred while attempting to find notification.", ex);
+                return NotFound(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while attempting to get positions.");
+                return Problem("Internal server error.");
+            }
         }
 
         [HttpDelete("{notificationId}")]
