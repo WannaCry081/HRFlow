1829e1cf0fe00edfae34cd57680aefcb82b334ee
shielamae02 <shiela.mlepon@gmail.com>
2023-11-22 23:49:06 +0800

    Add notification mapper

---
+++ backend/HRIS/Mappers/NotificationMapper.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using HRIS.Dtos.NotificationDto;
+using HRIS.Models;
+
+namespace HRIS.Mappers
+{
+    public class NotificationMapper : Profile
+    {
+        public NotificationMapper()
+        {
+            CreateMap<Notification, CreateNotificationDto>()
+                .ReverseMap();
+            CreateMap<Notification, GetNotificationDto>()
+                .ReverseMap();
+            CreateMap<Notification, UpdateNotificationDto>()
+                .ReverseMap();
+
+        }
+    }
+}
