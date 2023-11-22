6bd7f8d414c275836ff0460a6491b7fa333e0df3
shielamae02 <shiela.mlepon@gmail.com>
2023-11-22 23:56:47 +0800

    Add notification not found exception

---
+++ backend/HRIS/Exceptions/NotificationNotFoundException.cs
@@ -0,0 +1,7 @@
+﻿namespace HRIS.Exceptions
+{
+    public class NotificationNotFoundException : Exception
+    {
+        public NotificationNotFoundException(string message) : base(message) { }
+    }
+}
