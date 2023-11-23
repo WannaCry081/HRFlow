e4afb7a8abf39b211cfcaa6d790eb900f9cae75d
Gerald Carido <geraldcastcarido@gmail.com>
2023-11-24 01:50:08 +0800

    Add route url for notification

--- frontend/src/lib/constants.js
+++ frontend/src/lib/constants.js
@@ -7,3 +7,4 @@ export const EMPLOYEE_URL = BASE_URL + "/employee";
 export const USER_URL = BASE_URL + "/user";
 export const DEPARTMENT_URL = BASE_URL + "/department";
 export const RECORD_URL = BASE_URL + "/record";
+export const NOTIFICATION_URL = BASE_URL + "/notification";
