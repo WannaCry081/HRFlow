ecd3c2cc4de051d712ee6655f75f56235a127d5f
shielamae02 <shiela.mlepon@gmail.com>
2023-11-24 14:55:38 +0800

    Add textarea component

--- frontend/src/components/FormInput.jsx
+++ frontend/src/components/FormInput.jsx
@@ -1,49 +1,76 @@
 import { useState } from "react";
-import { VscEye, VscEyeClosed } from "react-icons/vsc"; 
+import { VscEye, VscEyeClosed } from "react-icons/vsc";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
 export const TextInput = (prop) => {
     return (
         <span>
-            <label htmlFor={prop.nameId} 
-                    className="block font-poppins mb-2 text-sm">{prop.name}
+            <label htmlFor={prop.nameId}
+                className="block font-poppins mb-2 text-sm">{prop.name}
                 {prop.required && <span className="text-red-500">*</span>}
             </label>
-            <input type={prop.type} 
-                    maxLength={prop.maxLength}
-                    minLength={prop.minLength}
-                    id={prop.nameId} 
-                    value={prop.value} 
-                    name={prop.nameId} 
-                    onChange={prop.onChange}
-                    readOnly={prop.readOnly}
-                    placeholder={prop.placeholder}
-                    onBlur={prop.onBlur}
-                    className="w-full border border-gray-200 rounded-md bg-gray-100 p-3 font-lato focus:border focus:outline-lilac sm:p-3 h-12 lg:h-14"/>
+            <input type={prop.type}
+                maxLength={prop.maxLength}
+                minLength={prop.minLength}
+                id={prop.nameId}
+                value={prop.value}
+                name={prop.nameId}
+                onChange={prop.onChange}
+                readOnly={prop.readOnly}
+                placeholder={prop.placeholder}
+                onBlur={prop.onBlur}
+                className="w-full border border-gray-200 rounded-md bg-gray-100 p-3 font-lato focus:border focus:outline-lilac sm:p-3 h-12 lg:h-14" />
 
             <div className="text-sm mt-2 text-start text-red-500 font-semibold">
-                {prop.errors && prop.touched && prop.errors }
+                {prop.errors && prop.touched && prop.errors}
+            </div>
+        </span>
+    );
+};
+
+export const TextArea = (prop) => {
+    return (
+        <span>
+            <label htmlFor={prop.nameId}
+                className="block font-poppins mb-2 text-sm">{prop.name}
+                {prop.required && <span className="text-red-500">*</span>}
+            </label>
+            <textarea type={prop.type}
+                maxLength={prop.maxLength}
+                minLength={prop.minLength}
+                id={prop.nameId}
+                value={prop.value}
+                name={prop.nameId}
+                rows={prop.rows || 5}
+                onChange={prop.onChange}
+                readOnly={prop.readOnly}
+                placeholder={prop.placeholder}
+                onBlur={prop.onBlur}
+                className="w-full border border-gray-200 rounded-md bg-gray-100 p-3 font-lato focus:border focus:outline-lilac sm:p-3  resize-none transition focus:border-gray-400 focus:outline-none font-normal h-full" />
+
+            <div className="text-sm mt-2 text-start text-red-500 font-semibold">
+                {prop.errors && prop.touched && prop.errors}
             </div>
         </span>
     );
 };
 
 export const PasswordInput = (prop) => {
-    const [showPassword, setShowPassword] = useState(false); 
+    const [showPassword, setShowPassword] = useState(false);
     const onSetShowPassword = (e) => {
         e.preventDefault();
         setShowPassword(!showPassword);
     };
-    
+
     return (
         <span>
             <label htmlFor={prop.nameId}
-                className="block font-poppins mb-2 text-sm">{prop.name} 
+                className="block font-poppins mb-2 text-sm">{prop.name}
                 <span className="text-red-500">*</span>
             </label>
             <div className="relative flex items-center">
-                <input type={ (showPassword) ? "text" : prop.type}
+                <input type={(showPassword) ? "text" : prop.type}
                     maxLength={prop.maxLength}
                     minLength={prop.minLength}
                     id={prop.nameId}
@@ -63,13 +90,13 @@ export const PasswordInput = (prop) => {
                 </button>
             </div>
             <div className="text-sm mt-2 text-start text-red-500 font-semibold">
-                {prop.errors && prop.touched && prop.errors }
+                {prop.errors && prop.touched && prop.errors}
             </div>
         </span>
     );
 };
 
-    
+
 export const CodeInput = (prop) => {
     return (
         <input className="w-full rounded-xl h-12 lg:h-14 sm:h-16 text-center text-xl border border-lilac bg-gray-100 font-poppins font-semibold focus:border focus:outline-lilac-light"
@@ -103,14 +130,14 @@ export const DatePickerInput = (prop) => {
                 className="block font-poppins mb-2 text-sm">{prop.name}
                 <span className="text-red-500">*</span>
             </label>
-            <DatePicker 
+            <DatePicker
                 id={prop.nameId}
                 name={prop.nameId}
                 placeholder={prop.placeholder}
                 selected={prop.selected}
-                onChange={prop.onChange} 
+                onChange={prop.onChange}
                 showYearDropdown
-                className="w-full border border-gray-200 rounded-md bg-gray-100 font-lato focus:border focus:outline-lilac-light p-4"/>
+                className="w-full border border-gray-200 rounded-md bg-gray-100 font-lato focus:border focus:outline-lilac-light p-4" />
         </span>
     );
 };
@@ -136,6 +163,6 @@ export const DropdownMenu = (prop) => {
                 {prop.errors && prop.touched && prop.errors}
             </div>
         </div>
-        
+
     );
 }
