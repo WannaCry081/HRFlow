9f0db68717d265e1181044d89a09fc8223215806
shielamae02 <shiela.mlepon@gmail.com>
2023-12-18 00:14:13 +0800

    Update notifications index implementation

--- frontend/src/pages/Dashboard/Notifications/index.jsx
+++ frontend/src/pages/Dashboard/Notifications/index.jsx
@@ -1,187 +1,127 @@
-import * as Yup from "yup";
-import { useFormik } from "formik";
 import { useState } from "react";
-import useToggle from "/src/hooks/useToggle";
-import useNotifications from "/src/hooks/useNotifications";
-import { CreateNotificationApi } from "/src/services/notificationService";
-import Toast from "/src/components/Toast";
-
-import { CircularProgressBar } from "/src/components/Loading";
-import { FaRegImage } from "react-icons/fa6";
-import { ScrollArea } from "/src/components/ui/scroll-area"
+import { useDeleteNotification, useNotifications } from "/src/hooks";
 import { Separator } from "/src/components/ui/separator"
-import ComputerChild from "/src/assets/svg/ComputerChild.svg";
-
-
-import {
-    Card,
-    CardContent,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "/src/components/ui/card";
-import {
-    TextInput,
-    TextArea,
-    SubmitButton,
-} from "/src/components/FormInput";
+import { PiTrashBold } from "react-icons/pi";
+import { FiEdit3 } from "react-icons/fi";
+import { IoMdAdd } from "react-icons/io";
+import Toast from "/src/components/Toast";
+import CreateNotification from "./Components/CreateNotification";
+import Arrow from "/src/assets/svg/Arrow.svg";
+import ConfirmModalBox from "/src/components/ConfirmModalBox";
 
 
 const Notifications = () => {
-    const [submit, onSetSubmit] = useToggle();
-    const [toast, onSetToast] = useToggle();
-    const notifications = useNotifications();
-    const token = sessionStorage.getItem("token");
 
-    const defaultImage = "/src/assets/svg/ComputerChild.svg";
+    const [createNotification, onSetCreateNotification] = useState(false);
+    const notifications = useNotifications(createNotification);
 
-    const formik = useFormik({
-        initialValues: {
-            subject: "",
-            message: "",
-            image: "", //imageName
-            imageSrc: defaultImage,
-            imageFile: null
-        },
+    const [openModal, onSetOpenModal] = useState(false);
+    const [selectNotification, onSetSelectNotification] = useState(null);
 
-        onSubmit: async (values) => {
-            onSetSubmit();
-            console.log(values);
+    const [deleteNotification, toast, confirmModal, onSetConfirmModal] =
+        useDeleteNotification(selectNotification, onSetCreateNotification, onSetSelectNotification);
 
-            const { status, data } = await CreateNotificationApi(token, values);
-
-            setTimeout(() => {
-                if (status === 200) {
-
-                    onSetToast();
-                    console.log("Creation successful!");
-
-                    setTimeout(() => {
-                        onSetToast();
-                    }, 1200);
-                } else if (status === 400) {
-                    formik.setErrors({
-                        subject: data
-                    });
-                } else {
-                    navigate("/error");
-                }
-                onSetSubmit();
-            }, 800)
-        },
-
-        validationSchema: Yup.object({
-            subject: Yup.string().required("Subject is required.")
-                .min(2, "Subject must be at least 2 characters.")
-                .max(50, "Subject can be at most 50 characters."),
-            message: Yup.string().required("Message is required.")
-                .min(2, "Message must be at least 2 characters.")
-                .max(100, "Message can be at most 100 characters."),
-        })
-    })
-
-    const showPreview = (e) => {
-        if(e.target.files && e.target.files[0]){
-            let imageFile = e.target.files[0];
-
-            const reader = new FileReader();
-            reader.onload = x => {
-                formik.setFieldValue("imageFile", imageFile);
-                formik.setFieldValue("imageSrc", x.target.result)
-            }
-            reader.readAsDataURL(imageFile)
-        } else {
-            formik.setFieldValue("imageFile", null);
-            formik.setFieldValue("imageSrc", defaultImage)
-        }
-    };
+    const formattedDate = new Date(selectNotification?.updatedAt).toLocaleString('en-US', {
+        weekday: 'short',
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: true
+    });
 
     return (
-        <section className="bg-lilac-pale h-full flex gap-3 p-8">
+        <section className="bg-lilac-pale h-full flex gap-5 p-8">
+            {toast && <Toast message="Notification successfullly deleted!" />}
+            {openModal && <CreateNotification
+                createNotification={createNotification}
+                selectNotification={selectNotification}
+                onSetOpenModal={onSetOpenModal}
+                onSetCreateNotification={onSetCreateNotification}
+                onCancel={() => {
+                    onSetOpenModal(false);
+                    onSetCreateNotification(false)
+                }}
+            />}
 
-            {toast && <Toast message="Notiification successfully added!" />}
-
-            <div className="w-[55%] p-2">
-                <CardTitle className="text-3xl font-semibold font-lato">Notifications List</CardTitle>
-                <Card className="h-[91%] mt-4 p-4">
-                    <ScrollArea className="h-[33rem] w-full">
-                        {notifications.map((notification) => (
-                            <NotificationCard
-                                key={notification.id}
-                                subject={notification.subject}
-                                message={notification.message}
-                                createdAt={notification.createdAt}
-                            />
-                        ))}
-                    </ScrollArea>
-                  
-                </Card>
+            {confirmModal && <ConfirmModalBox
+                onCancel={onSetConfirmModal}
+                header="Confirm Delete Notification"
+                operation="delete"
+                title={selectNotification.subject}
+                option="Delete"
+                submit={deleteNotification}
+            />}
+            <div className="flex flex-col min-w-[30rem] max-w-[30rem]">
+                <div className="flex justify-between mb-8">
+                    <h1 className="text-4xl font-bold font-lato text-lilac">Notifications</h1>
+                    <div onClick={() => {
+                        onSetOpenModal(true);
+                        onSetCreateNotification(true)
+                    }}
+                        className="px-6 gap-2 bg-lilac flex items-center rounded-lg justify-center py-3 hover:bg-lilac-dark h-full cursor-pointer">
+                        <IoMdAdd size={24} className="fill-white" />
+                        <p className="hidden lg:block text-white font-poppins font-semibold">
+                            Add notification
+                        </p>
+                    </div>
+                </div>
+                <div className="h-[38rem] overflow-scroll">
+                    {notifications.map((notification) => (
+                        <NotificationItem
+                            key={notification.id}
+                            onClick={(notification) => { onSetSelectNotification(notification) }}
+                            notification={notification}
+                            dateTime={notification.updatedAt}
+                            subject={notification.subject}
+                            message={notification.message}
+                        />
+                    ))}
+                </div>
             </div>
-            <div className="w-full self-center">
-                <Card className="w-full p-4 ">
-                    <CardHeader>
-                        <CardTitle className="text-3xl font-semibold font-lato text-lilac">Create Notification</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        <form onSubmit={formik.handleSubmit} >
-                            <img src={formik.values.imageSrc} className="h-40" />
-                            <div className="grid w-full items-center gap-4">
-                                <div className="flex flex-col space-y-1.5">
-                                    <TextInput nameId="subject"
-                                        required="required"
-                                        name="Subject"
-                                        type="text"
-                                        placeholder="Subject"
-                                        maxLength={100}
-                                        onBlur={formik.handleBlur}
-                                        errors={formik.errors.subject}
-                                        touched={formik.touched.subject}
-                                        onChange={formik.handleChange}
-                                        value={formik.values.subject} />
-                                    <TextArea nameId="message"
-                                        required="required"
-                                        name="Message"
-                                        type="text"
-                                        placeholder="Message"
-                                        maxLength={100}
-                                        onBlur={formik.handleBlur}
-                                        errors={formik.errors.message}
-                                        touched={formik.touched.message}
-                                        onChange={formik.handleChange}
-                                        rows={9}
-                                        value={formik.values.message} />
+            {/* Notification Full Preview */}
+            <div className="w-full h-full bg-white rounded-2xl py-12 px-8 flex flex-col">
+                {selectNotification || createNotification
+                    ? (
+                        <>
+                            <div className="flex justify-between items-center">
+                                <h1 className="text-lg font-poppins text-gray-500">
+                                    {formattedDate}
+                                </h1>
+                                <div className="flex gap-2">
+                                    <div onClick={() => {
+                                        onSetOpenModal(true);
+                                        onSetCreateNotification(false)
+                                    }}
+                                        className="rounded-full flex items-center justify-center bg-gray-200 hover:bg-pink-100 group p-3 h-14 w-14 cursor-pointer">
+                                        <FiEdit3 size={26} className="stroke-gray-500 group-hover:stroke-pink-500" />
+                                    </div>
+                                    <div onClick={() => onSetConfirmModal(true)}
+                                        className="rounded-full bg-gray-200 hover:bg-red-200 h-14 w-14 p-3 group cursor-pointer flex items-center justify-center">
+                                        <PiTrashBold size={26} className="fill-gray-500 group-hover:fill-red-500" />
+                                    </div>
                                 </div>
                             </div>
-                            <div className="flex justify-between mt-6 items-center">
-                                <div className="flex gap-2 cursor-pointer items-center">
-                                    <label htmlFor="imageUpload" className="flex items-center gap-2 hover:cursor-pointer">
-                                        <FaRegImage size={26} className="fill-lilac" />
-                                        <p className="text-lilac hover:underline text-lg">Add Image</p>
-                                    </label>
-                                    <input
-                                        id="imageUpload"
-                                        name="image"
-                                        type="file"
-                                        accept="image/*"
-                                        style={{ display: 'none' }}
-                                        onChange={showPreview}
-                                    />
-                                </div>
-                                <div className="w-1/4">
-                                    <SubmitButton>
-                                        {(submit) ? (
-                                            <CircularProgressBar>
-                                                <p className="ml-2 text-poppins text-white">Loading...</p>
-                                            </CircularProgressBar>
-                                        ) : (
-                                            <p className="text-poppins text-white">Submit</p>
-                                        )}
-                                    </SubmitButton>
-                                </div>
+                            <h1 className="text-lilac text-[2.5rem] font-bold my-4 leading-tight">
+                                {selectNotification?.subject}
+                            </h1>
+                            <p className="text-gray-800 py-2 text-lg">{selectNotification?.message}</p>
+                        </>
+                    ) : (
+                        <>
+                            <div className="h-full flex items-center justify-center flex-col">
+                                <img src={Arrow} alt="Arrow" className="h-20" />
+                                <h1 className="pt-4 text-2xl text-center text-rustic font-lato font-semibold">
+                                    No selected notification yet.
+                                </h1>
+                                <p className="text-lg font-poppins text-gray-500 text-center">
+                                    Click a notification to view content.
+                                </p>
                             </div>
-                        </form>
-                    </CardContent>
-                </Card>
+                        </>
+                    )}
             </div>
         </section>
     );
@@ -190,23 +130,31 @@ const Notifications = () => {
 export default Notifications;
 
 
-const NotificationCard = (prop) => {
-    return(
-        <div>
-            <div className="flex gap-2 p-2 relative bg-gray-50 rounded-tl-xl rounded-bl-xl cursor-pointer hover:bg-gray-100">
-                <div className="w-2 h-full absolute top-0 left-0 rounded-tl-xl rounded-bl-xl bg-lilac"/>
+export const NotificationItem = (prop) => {
+    const formatDateTime = (dateTimeString) => {
+        const options = { hour: 'numeric', minute: '2-digit', hour12: true };
+        return new Date(dateTimeString).toLocaleTimeString(undefined, options);
+    };
+    return (
+        <>
+            <div onClick={() => prop.onClick(prop.notification)}
+                className="flex w-full bg-gray-50 rounded-sm hover:bg-gray-100 py-2 relative max-w-1/3 cursor-pointer">
                 <div className="truncate w-full ml-2">
-                    <div className="flex flex-col justify-center h-full">
-                        <h1 className="font-semibold">
+                    <div className="w-1 h-full absolute rounded-tl-sm rounded-bl-sm top-0 left-0 bg-lilac" />
+                    <div className="flex flex-col justify-center h-full gap-1 ml-1">
+                        <h1 className="font-semibold text-lg">
                             {prop.subject}
                         </h1>
-                        <p className="text-gray-500 truncate">
-                            {prop.message}
-                        </p>
+                        <div className="flex justify-between">
+                            <p className="text-gray-500 truncate">
+                                {prop.message}
+                            </p>
+                            <p className="ml-5 mr-2">{formatDateTime(prop.dateTime)}</p>
+                        </div>
                     </div>
                 </div>
             </div>
             <Separator className="my-2" />
-        </div>
+        </>
     );
 }
