4cc905fec7a1bf9f66be1b5b4656d16958dc1f44
shielamae02 <shiela.mlepon@gmail.com>
2023-12-18 16:24:43 +0800

    Add fields for update user profile dto

--- backend/HRIS/dtos/UserDto/UpdateUserProfileDto.cs
+++ backend/HRIS/dtos/UserDto/UpdateUserProfileDto.cs
@@ -13,5 +13,11 @@ namespace HRIS.Dtos.UserDto
         [EmailAddress(ErrorMessage = "Please enter a valid personal email address.")]
         [StringLength(100, ErrorMessage = "Personal Email Address must be {1} characters long.")]
         public string PersonalEmail { get; set; } = string.Empty;
+
+        [StringLength(100, ErrorMessage = "Middle Name must be {1} characters long.")]
+        public string MiddleName { get; set; } = string.Empty;
+
+        [StringLength(1, ErrorMessage = "Suffix must be {1} characters long.")]
+        public string Suffix { get; set; } = string.Empty;
     }
 }
