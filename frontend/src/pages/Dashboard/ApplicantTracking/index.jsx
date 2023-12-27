c20606cbd83d37904537c01e66b7f0d39b649839
shielamae02 <shiela.mlepon@gmail.com>
2023-12-28 00:40:51 +0800

    Update main applicant page

--- frontend/src/pages/Dashboard/ApplicantTracking/index.jsx
+++ frontend/src/pages/Dashboard/ApplicantTracking/index.jsx
@@ -4,46 +4,47 @@ import { motion } from "framer-motion";
 import { LuSearch } from "react-icons/lu";
 import { IoMdAdd } from "react-icons/io";
 import useToggle from "/src/hooks/useToggle";
-import AddEmployeeForm from "./sample/AddEmployee";
-import ViewEmployee from "./sample/ViewEmployee";
+import ViewApplicant from "./Views/ViewApplicant";
 import Arrow from "/src/assets/svg/Arrow.svg";
+import AddApplicantForm from "./Views/AddApplicant";
 
 const ApplicantTracking = () => {
 
     const [submit, onSetSubmit] = useToggle();
 
-    const employees = useApplicants(submit);
-    const [selectEmployee, setSelectEmployee] = useState(null);
-    const [addEmployee, onSetAddEmployee] = useToggle();
+    const [selectApplicant, onSetSelectApplicant] = useState(null);
+    const [addApplicant, onSetAddApplicant] = useToggle();
     const [showCard, onSetShowCard] = useToggle();
     const [searchQuery, setSearchQuery] = useState("");
 
+    const applicants = useApplicants(addApplicant);
+
     const query = searchQuery ? searchQuery.toLowerCase() : "";
 
-    const groupedEmployees = employees.reduce((groups, employee) => {
-        const firstName = (employee.firstName || "").toLowerCase();
-        const lastName = employee.lastName.toLowerCase();
+    const groupedApplicants = applicants.reduce((groups, applicant) => {
+        const firstName = (applicant.firstName || "").toLowerCase();
+        const lastName = applicant.lastName.toLowerCase();
         const firstLetter = firstName[0].toUpperCase();
 
         if (firstName.includes(query) || lastName.includes(query)) {
             if (!groups[firstLetter]) {
                 groups[firstLetter] = [];
             }
-            groups[firstLetter].push(employee);
+            groups[firstLetter].push(applicant);
         }
 
         return groups;
     }, {});
 
-    const sortedGroupLetters = Object.keys(groupedEmployees).sort();
+    const sortedGroupLetters = Object.keys(groupedApplicants).sort();
 
     return (
         <div className="flex h-full">
-            {addEmployee
-                ? <AddEmployeeForm onSetSubmit={onSetSubmit} submit={submit} onSetAddEmployee={onSetAddEmployee} />
+            { addApplicant
+                ? <AddApplicantForm onSetSubmit={onSetSubmit} submit={submit} onSetAddApplicant={onSetAddApplicant} />
                 : <>
                     <section className="w-[75%] bg-lilac-pale rounded-t-[2.5rem] p-8 flex flex-col">
-                        <h1 className="text-4xl font-semibold font-lato">Applicant Tracking</h1>
+                        <h1 className="text-4xl font-bold font-lato text-lilac mt-4">Applicant Tracking</h1>
                         <div className="flex justify-between w-full items-center my-4">
                             <div className="relative flex items-center w-full">
                                 <LuSearch size={24} className="absolute ml-3 stroke-gray-400" />
@@ -55,7 +56,7 @@ const ApplicantTracking = () => {
                                 />
                             </div>
                             <div
-                                onClick={onSetAddEmployee}
+                                onClick={onSetAddApplicant}
                                 className="w-1/5 lg:w-56 gap-2 bg-lilac flex items-center rounded-lg justify-center hover:bg-lilac-dark h-full cursor-pointer">
                                 <IoMdAdd size={24} className="fill-white" />
                                 <p className="hidden lg:block text-white font-poppins font-semibold">
@@ -74,7 +75,7 @@ const ApplicantTracking = () => {
                                             Email Address
                                         </th>
                                         <th scope="col" className="text-lilac py-3 pt-6 px-6 hidden lg:table-cell xl:pl-6 w-[25%]">
-                                            Mobile Number
+                                            Status
                                         </th>
                                     </tr>
                                 </thead>
@@ -86,30 +87,30 @@ const ApplicantTracking = () => {
                                                     {groupLetter}
                                                 </td>
                                             </tr>
-                                            {groupedEmployees[groupLetter].map((employee) => (
+                                            {groupedApplicants[groupLetter].map((applicant) => (
                                                 <tr
-                                                    key={employee.id}
-                                                    className={`hover:bg-lilac-pastel cursor-pointer h-full ${selectEmployee && employee.id === selectEmployee.id ? "bg-lilac-pastel" : ""}`}
+                                                    key={applicant.id}
+                                                    className={`hover:bg-lilac-pastel cursor-pointer h-full ${selectApplicant && applicant.id === selectApplicant.id ? "bg-lilac-pastel" : ""}`}
                                                     onClick={() => {
-                                                        setSelectEmployee(employee);
+                                                        onSetSelectApplicant(applicant);
                                                         onSetShowCard();
                                                     }}
                                                 >
                                                     <td className="px-6 py-3 h-full font-poppins text-gray-600 flex items-center text-ellipsis">
                                                         <div className="font-medium h-14 w-14 flex justify-center items-center bg-blush-pastel mr-6 rounded-xl shadow-md">
                                                             <div className="text-blush text-xl font-poppins">
-                                                                {employee.firstName[0]}{employee.lastName[0]}
+                                                                {applicant.firstName[0]}{applicant.lastName[0]}
                                                             </div>
                                                         </div>
                                                         <div className="flex flex-col text-gray-800 font-poppins">
-                                                            {employee.firstName} {employee.lastName}
+                                                            {applicant.firstName} {applicant.lastName}
                                                         </div>
                                                     </td>
                                                     <td className="hidden md:table-cell py-2 font-poppins text-gray-500 w-[25%] truncate">
-                                                        {employee.email}
+                                                        {applicant.email}
                                                     </td>
                                                     <td className="hidden font-poppins lg:table-cell py-2 lg:pl-6 text-gray-500 w-[25%] truncate">
-                                                        {employee.mobileNumber || "N/A"}
+                                                        {applicant.status || "N/A"}
                                                     </td>
                                                 </tr>
                                             ))}
@@ -120,7 +121,7 @@ const ApplicantTracking = () => {
                         </div>
                     </section>
                     <section className="w-[25%] p-2">
-                        {selectEmployee == null
+                        {selectApplicant == null
                             ? <div className="h-full flex items-center justify-center flex-col">
                                 <img src={Arrow} alt="Arrow" className="h-20" />
                                 <h1 className="pt-4 text-2xl text-center text-rustic font-lato font-semibold">
@@ -131,9 +132,9 @@ const ApplicantTracking = () => {
                                 </p>
 
                             </div>
-                            : <ViewEmployee 
+                            : <ViewApplicant 
                                 submit={submit}
-                                selectEmployee={selectEmployee} />}
+                                selectApplicant={selectApplicant} />}
                     </section>
                 </>
             }
